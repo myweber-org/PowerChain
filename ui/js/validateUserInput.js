e88d2eb73c86d4a7caccd3c7d73e39b61c9e1d74
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-17 19:47:47 +0000

    Add input validation function to sanitize user-provided strings

--- ui/js/validateUserInput.js
+++ ui/js/validateUserInput.js
@@ -34,4 +34,40 @@ function validateUserInput(email, password) {
   };
 }
 
-export { validateEmail, validatePassword, validateUserInput };
+export { validateEmail, validatePassword, validateUserInput };function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    return input
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#x27;')
+        .replace(/\//g, '&#x2F;');
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    
+    return password.length >= minLength && 
+           hasUpperCase && 
+           hasLowerCase && 
+           hasNumbers && 
+           hasSpecialChar;
+}
+
+module.exports = {
+    sanitizeInput,
+    validateEmail,
+    validatePassword
+};
