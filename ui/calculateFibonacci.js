439a251305cd477863654361e32c34d6a2c3cd87
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-17 22:54:14 +0000

    Add function to calculate Fibonacci sequence up to n terms

---
+++ ui/calculateFibonacci.js
@@ -0,0 +1,12 @@
+function calculateFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    
+    let sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+        sequence.push(sequence[i-1] + sequence[i-2]);
+    }
+    return sequence;
+}
+
+module.exports = calculateFibonacci;
