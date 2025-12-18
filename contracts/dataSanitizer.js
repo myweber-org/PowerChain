9353ace7fa6d98004430b96da587130386499fd7
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-18 18:19:52 +0000

    Add data sanitization utility to prevent XSS attacks

---
+++ contracts/dataSanitizer.js
@@ -0,0 +1,23 @@
+function sanitizeInput(input) {
+    const div = document.createElement('div');
+    div.textContent = input;
+    return div.innerHTML;
+}
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
+
+function escapeHtml(text) {
+    const map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    };
+    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+
+export { sanitizeInput, validateEmail, escapeHtml };
