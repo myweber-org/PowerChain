43bb12c28f176f6ccb906f2f10519671cf37f2af
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-18 00:38:52 +0000

    Add input validation function for username and password

--- js/validateUserInput.js
+++ js/validateUserInput.js
@@ -60,4 +60,29 @@ function validateUserInput(username, email) {
     };
 }
 
-export { validateUsername, validateEmail, validateUserInput };
+export { validateUsername, validateEmail, validateUserInput };function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+    
+    if (!usernameValid && !passwordValid) {
+        return { valid: false, message: 'Username and password are invalid' };
+    } else if (!usernameValid) {
+        return { valid: false, message: 'Username is invalid' };
+    } else if (!passwordValid) {
+        return { valid: false, message: 'Password is invalid' };
+    } else {
+        return { valid: true, message: 'All inputs are valid' };
+    }
+}
+
+module.exports = { validateUserInput, validateUsername, validatePassword };
