9ebe5dd5044caa85847df52021f21ee052d3487a
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-20 18:58:36 +0000

    Add input validation utility for username and password fields

--- ui/validateUserInput.js
+++ ui/validateUserInput.js
@@ -137,4 +137,39 @@ module.exports = {
     validateUsername,
     validatePassword,
     validateUserInput
-};
+};function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+
+    return password.length >= minLength && 
+           hasUpperCase && 
+           hasLowerCase && 
+           hasNumbers && 
+           hasSpecialChar;
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+
+    if (!usernameValid && !passwordValid) {
+        return { valid: false, message: 'Invalid username and password' };
+    }
+    if (!usernameValid) {
+        return { valid: false, message: 'Invalid username' };
+    }
+    if (!passwordValid) {
+        return { valid: false, message: 'Invalid password' };
+    }
+    return { valid: true, message: 'Validation successful' };
+}
+
+export { validateUsername, validatePassword, validateUserInput };
