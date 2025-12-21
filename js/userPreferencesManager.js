cda9799d373a2fc6678e7f02da7c617ae405808b
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-21 00:06:36 +0000

    Add user preferences management with localStorage persistence

--- js/userPreferencesManager.js
+++ js/userPreferencesManager.js
@@ -118,4 +118,42 @@ const UserPreferencesManager = (function() {
     }
 };
 
-export default UserPreferences;
+export default UserPreferences;const UserPreferences = {
+  _prefs: {},
+
+  init() {
+    const stored = localStorage.getItem('userPrefs');
+    this._prefs = stored ? JSON.parse(stored) : {};
+  },
+
+  set(key, value) {
+    this._prefs[key] = value;
+    this._save();
+    return this;
+  },
+
+  get(key, defaultValue = null) {
+    return this._prefs.hasOwnProperty(key) ? this._prefs[key] : defaultValue;
+  },
+
+  remove(key) {
+    delete this._prefs[key];
+    this._save();
+    return this;
+  },
+
+  clear() {
+    this._prefs = {};
+    localStorage.removeItem('userPrefs');
+  },
+
+  getAll() {
+    return { ...this._prefs };
+  },
+
+  _save() {
+    localStorage.setItem('userPrefs', JSON.stringify(this._prefs));
+  }
+};
+
+UserPreferences.init();
