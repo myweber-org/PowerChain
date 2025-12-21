b5d55e86013fd9741b5e05030f9a7bcd12df2aad
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-21 20:06:11 +0000

    Add function to calculate the average of an array of numbers

---
+++ contracts/calculateAverage.js
@@ -0,0 +1,9 @@
+
+function calculateAverage(numbers) {
+    if (!Array.isArray(numbers) || numbers.length === 0) {
+        return 0;
+    }
+    
+    const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+    return sum / numbers.length;
+}
