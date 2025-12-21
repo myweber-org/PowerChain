94aeb7d6a59873b933fee1b185dbc7f252271723
InkspillVerse <inkspill.verse@outlook.com>
2025-12-21 23:43:47 +0000

    Add input sanitization utility to prevent XSS attacks

--- ui/js/vendor/dataSanitizer.js
+++ ui/js/vendor/dataSanitizer.js
@@ -32,4 +32,38 @@ function validateUserInput(input) {
     return sanitized.trim();
 }
 
-export { sanitizeInput, validateUserInput };
+export { sanitizeInput, validateUserInput };function sanitizeInput(input) {
+  if (typeof input !== 'string') {
+    return '';
+  }
+  
+  const map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#x27;',
+    '/': '&#x2F;'
+  };
+  
+  const reg = /[&<>"'/]/ig;
+  return input.replace(reg, (match) => map[match]);
+}
+
+function validateAndSanitize(userInput, maxLength = 1000) {
+  if (!userInput || typeof userInput !== 'string') {
+    return '';
+  }
+  
+  const trimmed = userInput.trim();
+  if (trimmed.length > maxLength) {
+    return sanitizeInput(trimmed.substring(0, maxLength));
+  }
+  
+  return sanitizeInput(trimmed);
+}
+
+module.exports = {
+  sanitizeInput,
+  validateAndSanitize
+};
