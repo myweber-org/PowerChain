5953607cdb1445f5bcc20ebf19a59cd2951fdeff
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-21 19:46:49 +0000

    Add function to calculate area of a circle given radius

---
+++ ui/build/calculateCircleArea.js
@@ -0,0 +1,8 @@
+function calculateCircleArea(radius) {
+    if (typeof radius !== 'number' || radius <= 0) {
+        throw new Error('Radius must be a positive number');
+    }
+    return Math.PI * Math.pow(radius, 2);
+}
+
+module.exports = calculateCircleArea;
