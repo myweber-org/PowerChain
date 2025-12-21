5ccf488facf6f7ad1ecd7e780e973372a5136aa7
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-21 20:35:45 +0000

    Add file management utility with create, read, and delete operations

---
+++ build/fileManager.js
@@ -0,0 +1,42 @@
+class FileManager {
+  constructor() {
+    this.files = new Map();
+  }
+
+  createFile(filename, content = '') {
+    if (this.files.has(filename)) {
+      throw new Error(`File ${filename} already exists`);
+    }
+    this.files.set(filename, content);
+    return { filename, content };
+  }
+
+  readFile(filename) {
+    if (!this.files.has(filename)) {
+      throw new Error(`File ${filename} not found`);
+    }
+    return this.files.get(filename);
+  }
+
+  deleteFile(filename) {
+    if (!this.files.has(filename)) {
+      throw new Error(`File ${filename} not found`);
+    }
+    this.files.delete(filename);
+    return true;
+  }
+
+  listFiles() {
+    return Array.from(this.files.keys());
+  }
+
+  searchFiles(pattern) {
+    const regex = new RegExp(pattern);
+    return this.listFiles().filter(filename => regex.test(filename));
+  }
+}
+
+const manager = new FileManager();
+manager.createFile('document.txt', 'Sample content');
+manager.createFile('data.json', '{"key": "value"}');
+console.log(manager.listFiles());
