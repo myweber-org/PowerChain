0bd2f826723b74d43bf41d22e83c82b3814f7447
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-23 17:34:03 +0000

    Add input validation function to sanitize user-provided strings

--- ui/js/vendor/validateUserInput.js
+++ ui/js/vendor/validateUserInput.js
@@ -349,4 +349,41 @@ function validateUserInput(username, email) {
     };
 }
 
-module.exports = validateUserInput;
+module.exports = validateUserInput;function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        throw new TypeError('Input must be a string');
+    }
+    
+    return input
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#x27;')
+        .replace(/\//g, '&#x2F;')
+        .trim();
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    if (password.length < 8) {
+        return false;
+    }
+    
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    
+    return hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar;
+}
+
+module.exports = {
+    sanitizeInput,
+    validateEmail,
+    validatePassword
+};
