ba30d61526735768ca447a4dd9035f78e24ed7e6
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-23 20:46:39 +0000

    Add input validation for username and password fields

--- contracts/validateUserInput.js
+++ contracts/validateUserInput.js
@@ -49,4 +49,34 @@ export { validateUsername, validateEmail, validateUserInput };function validateU
     };
 }
 
-module.exports = validateUserInput;
+module.exports = validateUserInput;function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    if (password.length < 8) {
+        return false;
+    }
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    return hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar;
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+    
+    if (!usernameValid && !passwordValid) {
+        return 'Invalid username and password';
+    } else if (!usernameValid) {
+        return 'Invalid username';
+    } else if (!passwordValid) {
+        return 'Invalid password';
+    }
+    return 'Valid input';
+}
+
+module.exports = { validateUserInput, validateUsername, validatePassword };
