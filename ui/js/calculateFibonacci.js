398c344093c1fdc12325f6d6b61789a950f9dcbb
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-23 19:02:31 +0000

    Add function to calculate Fibonacci sequence up to a given number

--- ui/js/calculateFibonacci.js
+++ ui/js/calculateFibonacci.js
@@ -7,4 +7,21 @@ function calculateFibonacci(n) {
         sequence.push(sequence[i - 1] + sequence[i - 2]);
     }
     return sequence;
+}function calculateFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    
+    let sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+        sequence.push(sequence[i-1] + sequence[i-2]);
+    }
+    return sequence;
 }
+
+function displayFibonacci(limit) {
+    const result = calculateFibonacci(limit);
+    console.log(`Fibonacci sequence up to ${limit} numbers:`, result);
+    return result;
+}
+
+module.exports = { calculateFibonacci, displayFibonacci };
