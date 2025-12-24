b79ee7c3d2f6e4482f39fd2b93b831d063727e8c
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-24 20:20:25 +0000

    Add email validation utility function

--- js/validateEmail.js
+++ js/validateEmail.js
@@ -3,25 +3,4 @@ function validateEmail(email) {
     return regex.test(email);
 }
 
-module.exports = validateEmail;function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
-
-module.exports = validateEmail;function validateEmail(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-}
-
-module.exports = validateEmail;function validateEmail(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-}
-
-module.exports = validateEmail;
-function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
-
 module.exports = validateEmail;
