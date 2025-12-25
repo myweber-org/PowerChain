a61ed357fec2802264a6f77865b941420ba08dc3
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-25 22:50:29 +0000

    Add function to calculate Fibonacci sequence up to a given number

---
+++ ui/calculateFibonacci.js
@@ -0,0 +1,16 @@
+function calculateFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    
+    const sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence;
+}
+
+function validateInput(input) {
+    return Number.isInteger(input) && input > 0;
+}
+
+module.exports = { calculateFibonacci, validateInput };
