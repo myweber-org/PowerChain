a7aff74386c3e4a128a78d9e4290f1c84519d0ec
InkspillVerse <inkspill.verse@outlook.com>
2025-12-25 16:44:40 +0000

    Add function to fetch and display user data from an API

--- ui/js/vendor/fetchUserData.js
+++ ui/js/vendor/fetchUserData.js
@@ -96,4 +96,36 @@ function displayUserInfo(user) {
             <p>Company: ${user.company.name}</p>
         `;
     }
+}function fetchUserData(userId) {
+    const apiUrl = `https://jsonplaceholder.typicode.com/users/${userId}`;
+    
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('User Data:', data);
+            displayUserInfo(data);
+        })
+        .catch(error => {
+            console.error('Error fetching user data:', error);
+        });
 }
+
+function displayUserInfo(user) {
+    const outputDiv = document.getElementById('userInfo');
+    if (outputDiv) {
+        outputDiv.innerHTML = `
+            <h3>${user.name}</h3>
+            <p>Email: ${user.email}</p>
+            <p>Phone: ${user.phone}</p>
+            <p>Website: ${user.website}</p>
+            <p>Company: ${user.company.name}</p>
+        `;
+    }
+}
+
+fetchUserData(1);
