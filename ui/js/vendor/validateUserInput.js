c3da5dbb864adf6f10e24ee16edeaccfcb0b16ec
InkspillVerse <inkspill.verse@outlook.com>
2025-12-26 15:58:16 +0000

    Add input validation for username and password fields

--- ui/js/vendor/validateUserInput.js
+++ ui/js/vendor/validateUserInput.js
@@ -156,4 +156,31 @@ function validatePassword(password) {
   return (hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar) ? sanitized : null;
 }
 
-export { sanitizeInput, validateEmail, validatePassword };
+export { sanitizeInput, validateEmail, validatePassword };function validateUserInput(username, password) {
+    const errors = [];
+
+    if (!username || username.trim() === '') {
+        errors.push('Username is required');
+    } else if (username.length < 3) {
+        errors.push('Username must be at least 3 characters long');
+    } else if (username.length > 20) {
+        errors.push('Username must be less than 20 characters');
+    }
+
+    if (!password || password.trim() === '') {
+        errors.push('Password is required');
+    } else if (password.length < 8) {
+        errors.push('Password must be at least 8 characters long');
+    } else if (!/[A-Z]/.test(password)) {
+        errors.push('Password must contain at least one uppercase letter');
+    } else if (!/[0-9]/.test(password)) {
+        errors.push('Password must contain at least one number');
+    } else if (!/[!@#$%^&*]/.test(password)) {
+        errors.push('Password must contain at least one special character');
+    }
+
+    return {
+        isValid: errors.length === 0,
+        errors: errors
+    };
+}
