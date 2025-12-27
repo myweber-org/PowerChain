aeaeecebdb827dc7eabbba5cd092d5fc08abbe86
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-27 22:25:04 +0000

    Add random color generator utility function

--- build/randomColorGenerator.js
+++ build/randomColorGenerator.js
@@ -1,18 +1,25 @@
+
 function generateRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
-function getRandomColorArray(count) {
-    const colors = [];
-    for (let i = 0; i < count; i++) {
-        colors.push(generateRandomColor());
-    }
-    return colors;
+function generateRandomRGBColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
 }
 
-export { generateRandomColor, getRandomColorArray };
+function generateRandomHSLColor() {
+  const h = Math.floor(Math.random() * 360);
+  const s = Math.floor(Math.random() * 101);
+  const l = Math.floor(Math.random() * 101);
+  return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
+export { generateRandomColor, generateRandomRGBColor, generateRandomHSLColor };
