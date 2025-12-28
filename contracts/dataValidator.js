5b8de37d25de3f2f0b2573d8367d0b1d1bdcf7e1
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-28 18:45:22 +0000

    Add data validation utility for user input sanitization

--- contracts/dataValidator.js
+++ contracts/dataValidator.js
@@ -29,4 +29,32 @@ function validateFormData(formData) {
     };
 }
 
-export { validateEmail, validatePhoneNumber, validateFormData };
+export { validateEmail, validatePhoneNumber, validateFormData };function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePhone(phone) {
+    const phoneRegex = /^\+?[\d\s\-\(\)]{10,}$/;
+    return phoneRegex.test(phone);
+}
+
+function sanitizeInput(input) {
+    return input.trim().replace(/[<>]/g, '');
+}
+
+function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*]/.test(password);
+    
+    return password.length >= minLength && 
+           hasUpperCase && 
+           hasLowerCase && 
+           hasNumbers && 
+           hasSpecialChar;
+}
+
+export { validateEmail, validatePhone, sanitizeInput, validatePassword };
