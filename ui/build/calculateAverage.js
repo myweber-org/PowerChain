20faa51d6401d2fcb9ac266fc60326e7e26f8698
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-30 20:01:34 +0000

    Add function to calculate average of an array of numbers

---
+++ ui/build/calculateAverage.js
@@ -0,0 +1,8 @@
+function calculateAverage(numbers) {
+    if (!Array.isArray(numbers) || numbers.length === 0) {
+        return 0;
+    }
+    
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
+    return sum / numbers.length;
+}
