4496d50898c13e04d6b6574a5952060199be2d7f
InkspillVerse <inkspill.verse@outlook.com>
2025-12-31 21:21:50 +0000

    Add user preferences manager with localStorage support

--- ui/js/userPreferencesManager.js
+++ ui/js/userPreferencesManager.js
@@ -179,4 +179,39 @@ export default UserPreferences;const UserPreferencesManager = (() => {
   };
 })();
 
-export default UserPreferencesManager;
+export default UserPreferencesManager;const UserPreferences = {
+  storageKey: 'app_preferences',
+
+  getPreferences() {
+    const stored = localStorage.getItem(this.storageKey);
+    return stored ? JSON.parse(stored) : {};
+  },
+
+  setPreference(key, value) {
+    const preferences = this.getPreferences();
+    preferences[key] = value;
+    localStorage.setItem(this.storageKey, JSON.stringify(preferences));
+    return true;
+  },
+
+  removePreference(key) {
+    const preferences = this.getPreferences();
+    if (preferences.hasOwnProperty(key)) {
+      delete preferences[key];
+      localStorage.setItem(this.storageKey, JSON.stringify(preferences));
+      return true;
+    }
+    return false;
+  },
+
+  clearAll() {
+    localStorage.removeItem(this.storageKey);
+    return true;
+  },
+
+  getAll() {
+    return this.getPreferences();
+  }
+};
+
+export default UserPreferences;
