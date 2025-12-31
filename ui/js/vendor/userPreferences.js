c5b646ce3f9d10f1c93637524e3a58bcb2441a5d
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-31 14:58:46 +0000

    Add user preferences validation and default settings

--- ui/js/vendor/userPreferences.js
+++ ui/js/vendor/userPreferences.js
@@ -46,4 +46,48 @@ function saveUserPreferences(preferences) {
     return validated;
 }
 
-export { validateUserPreferences, initializeUserSettings, saveUserPreferences };
+export { validateUserPreferences, initializeUserSettings, saveUserPreferences };const defaultPreferences = {
+  theme: 'light',
+  notifications: true,
+  language: 'en',
+  resultsPerPage: 25
+};
+
+function validatePreferences(userPrefs) {
+  const validPrefs = {};
+  
+  if (userPrefs.theme && ['light', 'dark', 'auto'].includes(userPrefs.theme)) {
+    validPrefs.theme = userPrefs.theme;
+  } else {
+    validPrefs.theme = defaultPreferences.theme;
+  }
+  
+  if (typeof userPrefs.notifications === 'boolean') {
+    validPrefs.notifications = userPrefs.notifications;
+  } else {
+    validPrefs.notifications = defaultPreferences.notifications;
+  }
+  
+  if (userPrefs.language && ['en', 'es', 'fr', 'de'].includes(userPrefs.language)) {
+    validPrefs.language = userPrefs.language;
+  } else {
+    validPrefs.language = defaultPreferences.language;
+  }
+  
+  if (Number.isInteger(userPrefs.resultsPerPage) && userPrefs.resultsPerPage > 0) {
+    validPrefs.resultsPerPage = Math.min(userPrefs.resultsPerPage, 100);
+  } else {
+    validPrefs.resultsPerPage = defaultPreferences.resultsPerPage;
+  }
+  
+  return validPrefs;
+}
+
+function mergeWithDefaults(preferences) {
+  return {
+    ...defaultPreferences,
+    ...validatePreferences(preferences)
+  };
+}
+
+export { validatePreferences, mergeWithDefaults, defaultPreferences };
