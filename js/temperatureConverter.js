76755cdcf62dfe451d00c868d3b72b8db58ebe97
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-02 20:54:02 +0000

    Add temperature conversion utility with Celsius to Fahrenheit function

--- js/temperatureConverter.js
+++ js/temperatureConverter.js
@@ -6,90 +6,15 @@ function fahrenheitToCelsius(fahrenheit) {
     return (fahrenheit - 32) * 5/9;
 }
 
-function convertTemperature(value, unit) {
-    if (unit.toLowerCase() === 'c') {
+function convertTemperature(value, fromUnit, toUnit) {
+    if (fromUnit === 'C' && toUnit === 'F') {
         return celsiusToFahrenheit(value);
-    } else if (unit.toLowerCase() === 'f') {
+    } else if (fromUnit === 'F' && toUnit === 'C') {
         return fahrenheitToCelsius(value);
+    } else if (fromUnit === toUnit) {
+        return value;
     } else {
-        throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
-    }
-}
-
-module.exports = {
-    celsiusToFahrenheit,
-    fahrenheitToCelsius,
-    convertTemperature
-};function celsiusToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-}
-
-function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-}
-
-function convertTemperature(value, unit) {
-    if (unit === 'C') {
-        return {
-            celsius: value,
-            fahrenheit: celsiusToFahrenheit(value)
-        };
-    } else if (unit === 'F') {
-        return {
-            celsius: fahrenheitToCelsius(value),
-            fahrenheit: value
-        };
-    }
-    throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
-}
-
-function formatTemperature(value, unit) {
-    const symbol = unit === 'C' ? '°C' : '°F';
-    return `${value.toFixed(1)}${symbol}`;
-}
-
-module.exports = {
-    celsiusToFahrenheit,
-    fahrenheitToCelsius,
-    convertTemperature,
-    formatTemperature
-};function celsiusToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-}
-
-function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-}
-
-function convertTemperature(value, unit) {
-    if (unit.toLowerCase() === 'c') {
-        return celsiusToFahrenheit(value);
-    } else if (unit.toLowerCase() === 'f') {
-        return fahrenheitToCelsius(value);
-    } else {
-        throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
-    }
-}
-
-module.exports = {
-    celsiusToFahrenheit,
-    fahrenheitToCelsius,
-    convertTemperature
-};function celsiusToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-}
-
-function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-}
-
-function convertTemperature(value, unit) {
-    if (unit.toLowerCase() === 'c') {
-        return celsiusToFahrenheit(value);
-    } else if (unit.toLowerCase() === 'f') {
-        return fahrenheitToCelsius(value);
-    } else {
-        throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
+        throw new Error('Unsupported temperature conversion');
     }
 }
 
