b361229f77f7446d9eb7e8eca2594a63e0fd1567
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-02 23:52:31 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and Kelvin functions

--- ui/build/temperatureConverter.js
+++ ui/build/temperatureConverter.js
@@ -20,4 +20,35 @@ module.exports = {
     celsiusToFahrenheit,
     fahrenheitToCelsius,
     convertTemperature
+};function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
+function celsiusToKelvin(celsius) {
+  return celsius + 273.15;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5/9;
+}
+
+function fahrenheitToKelvin(fahrenheit) {
+  return (fahrenheit - 32) * 5/9 + 273.15;
+}
+
+function kelvinToCelsius(kelvin) {
+  return kelvin - 273.15;
+}
+
+function kelvinToFahrenheit(kelvin) {
+  return (kelvin - 273.15) * 9/5 + 32;
+}
+
+export {
+  celsiusToFahrenheit,
+  celsiusToKelvin,
+  fahrenheitToCelsius,
+  fahrenheitToKelvin,
+  kelvinToCelsius,
+  kelvinToFahrenheit
 };
