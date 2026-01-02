3bbecb123093d23dae14c2bfdcf3d12813f8b8e4
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-02 14:23:42 +0000

    Add function to generate Fibonacci sequence up to a given number

--- build/calculateFibonacci.js
+++ build/calculateFibonacci.js
@@ -1,18 +1,7 @@
-function calculateFibonacci(n) {
-    if (n <= 0) return [];
-    if (n === 1) return [0];
-    
+function generateFibonacci(limit) {
     const sequence = [0, 1];
-    for (let i = 2; i < n; i++) {
-        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= limit) {
+        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
     }
-    return sequence.slice(0, n);
+    return sequence;
 }
-
-function printFibonacci(n) {
-    const result = calculateFibonacci(n);
-    console.log(`Fibonacci sequence (${n} terms):`, result);
-    return result;
-}
-
-export { calculateFibonacci, printFibonacci };
