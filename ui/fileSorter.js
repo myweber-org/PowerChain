c4ef5ec9b66a9278284a53371a5937a2e04d3a33
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-03 15:40:18 +0000

    Add utility function to sort files by extension and size

---
+++ ui/fileSorter.js
@@ -0,0 +1,10 @@
+function sortFiles(files) {
+    return files.sort((a, b) => {
+        const extA = a.name.split('.').pop().toLowerCase();
+        const extB = b.name.split('.').pop().toLowerCase();
+        if (extA !== extB) {
+            return extA.localeCompare(extB);
+        }
+        return a.size - b.size;
+    });
+}
