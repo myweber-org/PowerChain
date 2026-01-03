84b04bbcad6c14efff7342b75651b499501d86cf
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-03 23:54:31 +0000

    Add input validation function to sanitize user-provided strings

--- ui/js/validateUserInput.js
+++ ui/js/validateUserInput.js
@@ -1,6 +1,17 @@
-function validateUsername(username) {
-    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
-    return usernameRegex.test(username);
+function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        throw new TypeError('Input must be a string');
+    }
+    
+    const trimmed = input.trim();
+    const escaped = trimmed
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#x27;');
+    
+    return escaped;
 }
 
 function validateEmail(email) {
@@ -8,21 +19,18 @@ function validateEmail(email) {
     return emailRegex.test(email);
 }
 
-function validateUserInput(username, email) {
-    const errors = [];
+function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
     
-    if (!validateUsername(username)) {
-        errors.push('Username must be 3-20 characters and contain only letters, numbers, and underscores');
-    }
-    
-    if (!validateEmail(email)) {
-        errors.push('Please enter a valid email address');
-    }
-    
-    return {
-        isValid: errors.length === 0,
-        errors: errors
-    };
+    return password.length >= minLength && 
+           hasUpperCase && 
+           hasLowerCase && 
+           hasNumbers && 
+           hasSpecialChar;
 }
 
-export { validateUsername, validateEmail, validateUserInput };
+module.exports = { sanitizeInput, validateEmail, validatePassword };
