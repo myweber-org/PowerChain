bf5e7747481d75b4cb00cfcf8aaa8f631252df5c
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-03 16:12:11 +0000

    Add form validation for email and password fields

--- ui/formValidation.js
+++ ui/formValidation.js
@@ -16,9 +16,3 @@ function validateForm() {
 
     return true;
 }
-
-document.getElementById('submitBtn').addEventListener('click', function(event) {
-    if (!validateForm()) {
-        event.preventDefault();
-    }
-});
