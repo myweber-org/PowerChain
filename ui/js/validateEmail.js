87b904b329d7920b391d7ef22b9486cc904dde7c
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-04 14:43:25 +0000

    Add email validation utility function

--- ui/js/validateEmail.js
+++ ui/js/validateEmail.js
@@ -1,4 +1,4 @@
 function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
 }
