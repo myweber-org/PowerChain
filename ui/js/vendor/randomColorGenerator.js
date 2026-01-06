d46dfc613c372c6a2eeaebe899c428031e0268a7
InkspillVerse <inkspill.verse@outlook.com>
2026-01-06 16:47:52 +0000

    Add random color generator utility

--- ui/js/vendor/randomColorGenerator.js
+++ ui/js/vendor/randomColorGenerator.js
@@ -44,4 +44,21 @@ function generateRandomHSL() {
   return `hsl(${h}, ${s}%, ${l}%)`;
 }
 
-export { generateRandomColor, generateRandomRGB, generateRandomHSL };
+export { generateRandomColor, generateRandomRGB, generateRandomHSL };function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function generateRandomColors(count) {
+    const colors = [];
+    for (let i = 0; i < count; i++) {
+        colors.push(getRandomColor());
+    }
+    return colors;
+}
+
+export { getRandomColor, generateRandomColors };
