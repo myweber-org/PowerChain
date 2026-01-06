ec7169362ebe331791e5aa66e0ba684d7af1ddf2
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-06 21:05:29 +0000

    Add input validation function to sanitize user-provided strings

--- ui/js/vendor/validateUserInput.js
+++ ui/js/vendor/validateUserInput.js
@@ -128,4 +128,29 @@ function validateUserInput(email, password) {
     };
 }
 
-module.exports = { validateUserInput, validateEmail, validatePassword };
+module.exports = { validateUserInput, validateEmail, validatePassword };function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    return input.trim()
+        .replace(/[<>]/g, '')
+        .substring(0, 255);
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    return password.length >= 8 &&
+           /[A-Z]/.test(password) &&
+           /[a-z]/.test(password) &&
+           /\d/.test(password);
+}
+
+module.exports = {
+    sanitizeInput,
+    validateEmail,
+    validatePassword
+};
