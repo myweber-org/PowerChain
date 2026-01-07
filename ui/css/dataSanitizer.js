65e47354599f767e3f334d38455466c6f850b68c
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-07 00:17:00 +0000

    Add data sanitization utility to prevent XSS attacks

--- ui/css/dataSanitizer.js
+++ ui/css/dataSanitizer.js
@@ -20,4 +20,26 @@ function validateAndSanitize(userInput) {
     return sanitized;
 }
 
-module.exports = { validateAndSanitize };
+module.exports = { validateAndSanitize };function sanitizeInput(input) {
+    const div = document.createElement('div');
+    div.textContent = input;
+    return div.innerHTML;
+}
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
+
+function escapeHtml(text) {
+    const map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    };
+    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+
+export { sanitizeInput, validateEmail, escapeHtml };
