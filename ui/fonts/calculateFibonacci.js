084d2c43baf718d3254071a33a94d0142ac42278
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-08 15:19:03 +0000

    Add function to calculate Fibonacci sequence up to n terms

--- ui/fonts/calculateFibonacci.js
+++ ui/fonts/calculateFibonacci.js
@@ -9,4 +9,13 @@ function calculateFibonacci(limit) {
     if (n <= 2) return 1;
     memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
     return memo[n];
+}function calculateFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    
+    const sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence;
 }
