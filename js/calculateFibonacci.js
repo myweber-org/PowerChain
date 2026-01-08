f620e3e951c8db3d3ca790e9fd6f116b23990388
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-08 16:18:55 +0000

    Add function to generate Fibonacci sequence up to a given number

--- js/calculateFibonacci.js
+++ js/calculateFibonacci.js
@@ -50,4 +50,17 @@ function isFibonacciNumber(num) {
     return b === num || num === 0;
 }
 
-module.exports = { calculateFibonacci, isFibonacciNumber };
+module.exports = { calculateFibonacci, isFibonacciNumber };function calculateFibonacci(limit) {
+    if (limit <= 0) return [];
+    if (limit === 1) return [0];
+
+    const sequence = [0, 1];
+    while (true) {
+        const nextValue = sequence[sequence.length - 1] + sequence[sequence.length - 2];
+        if (nextValue > limit) break;
+        sequence.push(nextValue);
+    }
+    return sequence;
+}
+
+console.log(calculateFibonacci(100));
