e26142bcd077b5851bcc6ded978c35416535c60f
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-08 21:23:08 +0000

    Add file management utility for reading and writing JSON data

---
+++ js/fileManager.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+
+class FileManager {
+    constructor(basePath) {
+        this.basePath = basePath;
+    }
+
+    readJSON(filename) {
+        const filePath = path.join(this.basePath, filename);
+        try {
+            const data = fs.readFileSync(filePath, 'utf8');
+            return JSON.parse(data);
+        } catch (error) {
+            if (error.code === 'ENOENT') {
+                console.log(`File ${filename} not found. Returning empty object.`);
+                return {};
+            }
+            throw error;
+        }
+    }
+
+    writeJSON(filename, data) {
+        const filePath = path.join(this.basePath, filename);
+        const dir = path.dirname(filePath);
+        
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        
+        const jsonData = JSON.stringify(data, null, 2);
+        fs.writeFileSync(filePath, jsonData, 'utf8');
+        console.log(`Data written to ${filename} successfully.`);
+    }
+
+    mergeJSON(filename, newData) {
+        const existingData = this.readJSON(filename);
+        const mergedData = { ...existingData, ...newData };
+        this.writeJSON(filename, mergedData);
+        return mergedData;
+    }
+}
+
+module.exports = FileManager;
