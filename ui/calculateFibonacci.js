f0bb00e9cd8def976a4d2c53a9bb0839dc3e1935
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-09 14:25:12 +0000

    Add function to calculate Fibonacci sequence up to a given number

--- ui/calculateFibonacci.js
+++ ui/calculateFibonacci.js
@@ -5,4 +5,21 @@ function fibonacci(n, memo = {}) {
     return memo[n];
 }
 
-module.exports = fibonacci;
+module.exports = fibonacci;function calculateFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    
+    const sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence;
+}
+
+function displayFibonacci(limit) {
+    const result = calculateFibonacci(limit);
+    console.log(`Fibonacci sequence up to ${limit} numbers:`, result);
+    return result;
+}
+
+module.exports = { calculateFibonacci, displayFibonacci };
