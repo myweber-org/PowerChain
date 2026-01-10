3d96c7471099b5ba85aa868cd3bc0db1b07edf41
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-10 18:55:47 +0000

    Add input sanitization function to prevent XSS attacks

---
+++ ui/dataSanitizer.js
@@ -0,0 +1,12 @@
+function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    return input
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#x27;')
+        .replace(/\//g, '&#x2F;');
+}
