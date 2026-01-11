a8c5d5aab57e9bbf09497739c44f36036eb028a7
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-11 00:44:08 +0000

    Add input validation function to sanitize user-provided strings

--- build/validateUserInput.js
+++ build/validateUserInput.js
@@ -27,4 +27,19 @@ module.exports = {
     validateUsername,
     validatePassword,
     validateUserInput
-};
+};function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    return input
+        .replace(/[<>]/g, '')
+        .trim()
+        .substring(0, 255);
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+module.exports = { sanitizeInput, validateEmail };
