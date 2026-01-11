bf2680c9b98c0a4f3c947c696daad7ab4195d2e6
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-11 14:49:12 +0000

    Add input validation function to sanitize user-provided strings

--- build/validateUserInput.js
+++ build/validateUserInput.js
@@ -1,39 +1,9 @@
-function validateUsername(username) {
-    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
-    return usernameRegex.test(username);
-}
-
-function validatePassword(password) {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    return passwordRegex.test(password);
-}
-
-function validateUserInput(username, password) {
-    const usernameValid = validateUsername(username);
-    const passwordValid = validatePassword(password);
-    
-    if (!usernameValid) {
-        throw new Error('Invalid username format');
-    }
-    
-    if (!passwordValid) {
-        throw new Error('Invalid password format');
-    }
-    
-    return true;
-}
-
-module.exports = {
-    validateUsername,
-    validatePassword,
-    validateUserInput
-};function sanitizeInput(input) {
+function sanitizeInput(input) {
     if (typeof input !== 'string') {
         return '';
     }
-    return input
+    return input.trim()
         .replace(/[<>]/g, '')
-        .trim()
         .substring(0, 255);
 }
 
@@ -42,4 +12,11 @@ function validateEmail(email) {
     return emailRegex.test(email);
 }
 
-module.exports = { sanitizeInput, validateEmail };
+function validatePassword(password) {
+    return password.length >= 8 &&
+           /[A-Z]/.test(password) &&
+           /[a-z]/.test(password) &&
+           /\d/.test(password);
+}
+
+module.exports = { sanitizeInput, validateEmail, validatePassword };
