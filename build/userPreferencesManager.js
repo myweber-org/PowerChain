87a3b4c59d8b6a1d26408d114e71fc387993cef2
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-12 23:59:36 +0000

    Add user preferences management with localStorage support

--- build/userPreferencesManager.js
+++ build/userPreferencesManager.js
@@ -415,4 +415,48 @@ export default UserPreferencesManager;const UserPreferencesManager = (() => {
         reset: resetToDefaults,
         subscribe: subscribe
     };
+})();const userPreferencesManager = (function() {
+    const PREFERENCES_KEY = 'app_preferences';
+    
+    function getPreferences() {
+        const stored = localStorage.getItem(PREFERENCES_KEY);
+        return stored ? JSON.parse(stored) : {};
+    }
+    
+    function setPreference(key, value) {
+        const preferences = getPreferences();
+        preferences[key] = value;
+        localStorage.setItem(PREFERENCES_KEY, JSON.stringify(preferences));
+        return preferences;
+    }
+    
+    function removePreference(key) {
+        const preferences = getPreferences();
+        delete preferences[key];
+        localStorage.setItem(PREFERENCES_KEY, JSON.stringify(preferences));
+        return preferences;
+    }
+    
+    function clearPreferences() {
+        localStorage.removeItem(PREFERENCES_KEY);
+        return {};
+    }
+    
+    function hasPreference(key) {
+        const preferences = getPreferences();
+        return key in preferences;
+    }
+    
+    function getAllPreferences() {
+        return getPreferences();
+    }
+    
+    return {
+        get: getPreferences,
+        set: setPreference,
+        remove: removePreference,
+        clear: clearPreferences,
+        has: hasPreference,
+        all: getAllPreferences
+    };
 })();
