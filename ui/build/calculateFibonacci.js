5628dba5c8c63f67961d03e21d36e3cb8fd4879f
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-12 19:38:47 +0000

    Add function to compute Fibonacci sequence up to a given number

--- ui/build/calculateFibonacci.js
+++ ui/build/calculateFibonacci.js
@@ -1,6 +1,10 @@
-function calculateFibonacci(n, memo = {}) {
-    if (n in memo) return memo[n];
-    if (n <= 2) return 1;
-    memo[n] = calculateFibonacci(n - 1, memo) + calculateFibonacci(n - 2, memo);
-    return memo[n];
+function calculateFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    
+    let sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence;
 }
