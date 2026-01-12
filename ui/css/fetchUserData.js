53f12ab2793a2f6d460fd6dbd178fb0daae2b4aa
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-12 15:52:52 +0000

    Add function to fetch and display user data from API

--- ui/css/fetchUserData.js
+++ ui/css/fetchUserData.js
@@ -6,4 +6,19 @@ async function fetchUserData() {
   });
 }
 
-fetchUserData();
+fetchUserData();function fetchUserData(userId) {
+  return fetch(`https://api.example.com/users/${userId}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('User data:', data);
+      return data;
+    })
+    .catch(error => {
+      console.error('Error fetching user data:', error);
+    });
+}
