58240688b7f27cd8d638a58b28757a40de58a8c8
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-12 00:29:01 +0000

    Add function to calculate average of an array of numbers

--- ui/css/calculateAverage.js
+++ ui/css/calculateAverage.js
@@ -8,4 +8,14 @@ function calculateAverage(numbers) {
     return sum / numbers.length;
 }
 
-module.exports = calculateAverage;
+module.exports = calculateAverage;function calculateAverage(numbers) {
+    if (!Array.isArray(numbers) || numbers.length === 0) {
+        return 0;
+    }
+    
+    const sum = numbers.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    }, 0);
+    
+    return sum / numbers.length;
+}
