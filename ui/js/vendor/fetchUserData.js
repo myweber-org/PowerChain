88b38612ac58dad60d2918501134a60de9d68cc8
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-12 15:23:13 +0000

    Add fetchUserData function to retrieve and process user data from API

--- ui/js/vendor/fetchUserData.js
+++ ui/js/vendor/fetchUserData.js
@@ -179,4 +179,20 @@ function displayUserInfo(user) {
     }
 }
 
-fetchUserData(1);
+fetchUserData(1);function fetchUserData(userId) {
+  return fetch(`https://api.example.com/users/${userId}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('User data fetched:', data);
+      return data;
+    })
+    .catch(error => {
+      console.error('Error fetching user data:', error);
+      throw error;
+    });
+}
