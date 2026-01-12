bc624442f5c08a581356892d2de1042aa210ed9d
InkspillVerse <inkspill.verse@outlook.com>
2026-01-12 16:17:22 +0000

    Add function to compute the nth Fibonacci number using memoization

--- ui/build/calculateFibonacci.js
+++ ui/build/calculateFibonacci.js
@@ -1,21 +1,6 @@
-function calculateFibonacci(limit) {
-    if (limit <= 0) return [];
-    if (limit === 1) return [0];
-    
-    let sequence = [0, 1];
-    while (true) {
-        let nextValue = sequence[sequence.length - 1] + sequence[sequence.length - 2];
-        if (nextValue > limit) break;
-        sequence.push(nextValue);
-    }
-    return sequence;
-}function calculateFibonacci(n) {
-    if (n <= 0) return [];
-    if (n === 1) return [0];
-    
-    const sequence = [0, 1];
-    for (let i = 2; i < n; i++) {
-        sequence.push(sequence[i - 1] + sequence[i - 2]);
-    }
-    return sequence;
+function calculateFibonacci(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = calculateFibonacci(n - 1, memo) + calculateFibonacci(n - 2, memo);
+    return memo[n];
 }
