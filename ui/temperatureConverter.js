50e165ca3b022aeea47ac41f304000494256116d
InkspillVerse <inkspill.verse@outlook.com>
2026-01-14 23:01:53 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and vice versa

--- ui/temperatureConverter.js
+++ ui/temperatureConverter.js
@@ -16,8 +16,14 @@ function convertTemperature(value, unit) {
     }
 }
 
+function formatTemperature(value, unit) {
+    const symbol = unit.toUpperCase() === 'C' ? '°C' : '°F';
+    return `${value.toFixed(1)}${symbol}`;
+}
+
 module.exports = {
     celsiusToFahrenheit,
     fahrenheitToCelsius,
-    convertTemperature
+    convertTemperature,
+    formatTemperature
 };
