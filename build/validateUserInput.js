c6a5b46f147fae293809ac007526b6fb33ecf13d
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-14 23:38:42 +0000

    Add input validation function for username and password

--- build/validateUserInput.js
+++ build/validateUserInput.js
@@ -148,4 +148,34 @@ function validateUserInput(userData) {
     };
 }
 
-module.exports = validateUserInput;
+module.exports = validateUserInput;function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    if (password.length < 8) return false;
+    if (!/[A-Z]/.test(password)) return false;
+    if (!/[a-z]/.test(password)) return false;
+    if (!/[0-9]/.test(password)) return false;
+    if (!/[!@#$%^&*]/.test(password)) return false;
+    return true;
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+    
+    if (!usernameValid && !passwordValid) {
+        return { valid: false, message: 'Invalid username and password' };
+    }
+    if (!usernameValid) {
+        return { valid: false, message: 'Invalid username' };
+    }
+    if (!passwordValid) {
+        return { valid: false, message: 'Invalid password' };
+    }
+    return { valid: true, message: 'Validation successful' };
+}
+
+export { validateUserInput, validateUsername, validatePassword };
