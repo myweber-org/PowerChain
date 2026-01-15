3a6eab16bd0d0170f7f149dedc18cbec75965381
InkspillVerse <inkspill.verse@outlook.com>
2026-01-15 00:19:20 +0000

    Add function to compute Fibonacci sequence up to n terms

--- ui/calculateFibonacci.js
+++ ui/calculateFibonacci.js
@@ -22,4 +22,12 @@ function displayFibonacci(limit) {
     return result;
 }
 
-module.exports = { calculateFibonacci, displayFibonacci };
+module.exports = { calculateFibonacci, displayFibonacci };function calculateFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    let sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence;
+}
