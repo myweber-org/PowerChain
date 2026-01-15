f20084c6c8605f078b585ff8b4c36baa8436dc83
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-15 17:05:42 +0000

    Add data validation utility for user input sanitization

--- ui/build/dataValidator.js
+++ ui/build/dataValidator.js
@@ -3,23 +3,19 @@ function validateEmail(email) {
     return emailRegex.test(email);
 }
 
-function validatePhone(phone) {
-    const phoneRegex = /^\+?[\d\s\-\(\)]{10,}$/;
-    return phoneRegex.test(phone);
+function validatePassword(password) {
+    return password.length >= 8 &&
+           /[A-Z]/.test(password) &&
+           /[a-z]/.test(password) &&
+           /\d/.test(password);
 }
 
 function sanitizeInput(input) {
     return input.trim()
-        .replace(/[<>]/g, '')
-        .substring(0, 255);
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;')
+                .replace(/"/g, '&quot;')
+                .replace(/'/g, '&#x27;');
 }
 
-function validatePassword(password) {
-    if (password.length < 8) return false;
-    if (!/[A-Z]/.test(password)) return false;
-    if (!/[a-z]/.test(password)) return false;
-    if (!/\d/.test(password)) return false;
-    return true;
-}
-
-export { validateEmail, validatePhone, sanitizeInput, validatePassword };
+export { validateEmail, validatePassword, sanitizeInput };
