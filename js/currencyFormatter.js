ad19a4c6575e063c14b7cce2eec913cd6b5e869d
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-16 19:30:25 +0000

    Add utility function to format currency with locale support

--- js/currencyFormatter.js
+++ js/currencyFormatter.js
@@ -28,4 +28,24 @@ function parseCurrency(formattedString, locale = 'en-US') {
     return isNaN(parsedValue) ? 0 : parsedValue;
 }
 
+export { formatCurrency, parseCurrency };function formatCurrency(amount, locale = 'en-US', currency = 'USD') {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency
+  }).format(amount);
+}
+
+function parseCurrency(formattedString, locale = 'en-US') {
+  const example = formatCurrency(0, locale);
+  const decimalSeparator = example.replace(/\d/g, '').charAt(1);
+  const groupSeparator = example.replace(/\d/g, '').charAt(0);
+  
+  const cleanString = formattedString
+    .replace(new RegExp('\\' + groupSeparator, 'g'), '')
+    .replace(new RegExp('\\' + decimalSeparator), '.')
+    .replace(/[^\d.-]/g, '');
+  
+  return parseFloat(cleanString);
+}
+
 export { formatCurrency, parseCurrency };
