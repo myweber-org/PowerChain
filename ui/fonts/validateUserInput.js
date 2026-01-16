605697fa28b53d715c81c9c88c86b457d6b1a3f3
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-16 01:28:50 +0000

    Add input validation function for username and password

--- ui/fonts/validateUserInput.js
+++ ui/fonts/validateUserInput.js
@@ -1,62 +1,4 @@
 function validateUsername(username) {
-    const minLength = 3;
-    const maxLength = 20;
-    const regex = /^[a-zA-Z0-9_]+$/;
-    
-    if (typeof username !== 'string') {
-        return { valid: false, message: 'Username must be a string' };
-    }
-    
-    if (username.length < minLength) {
-        return { valid: false, message: `Username must be at least ${minLength} characters long` };
-    }
-    
-    if (username.length > maxLength) {
-        return { valid: false, message: `Username must not exceed ${maxLength} characters` };
-    }
-    
-    if (!regex.test(username)) {
-        return { valid: false, message: 'Username can only contain letters, numbers, and underscores' };
-    }
-    
-    return { valid: true, message: 'Username is valid' };
-}
-
-function validatePassword(password) {
-    const minLength = 8;
-    const hasUpperCase = /[A-Z]/;
-    const hasLowerCase = /[a-z]/;
-    const hasNumbers = /\d/;
-    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/;
-    
-    if (typeof password !== 'string') {
-        return { valid: false, message: 'Password must be a string' };
-    }
-    
-    if (password.length < minLength) {
-        return { valid: false, message: `Password must be at least ${minLength} characters long` };
-    }
-    
-    if (!hasUpperCase.test(password)) {
-        return { valid: false, message: 'Password must contain at least one uppercase letter' };
-    }
-    
-    if (!hasLowerCase.test(password)) {
-        return { valid: false, message: 'Password must contain at least one lowercase letter' };
-    }
-    
-    if (!hasNumbers.test(password)) {
-        return { valid: false, message: 'Password must contain at least one number' };
-    }
-    
-    if (!hasSpecialChar.test(password)) {
-        return { valid: false, message: 'Password must contain at least one special character' };
-    }
-    
-    return { valid: true, message: 'Password is valid' };
-}
-
-export { validateUsername, validatePassword };function validateUsername(username) {
     const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
     return usernameRegex.test(username);
 }
@@ -65,13 +7,13 @@ function validatePassword(password) {
     const minLength = 8;
     const hasUpperCase = /[A-Z]/.test(password);
     const hasLowerCase = /[a-z]/.test(password);
-    const hasNumbers = /\d/.test(password);
-    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
     
     return password.length >= minLength && 
            hasUpperCase && 
            hasLowerCase && 
-           hasNumbers && 
+           hasNumber && 
            hasSpecialChar;
 }
 
@@ -86,4 +28,4 @@ function validateUserInput(username, password) {
     };
 }
 
-export { validateUserInput, validateUsername, validatePassword };
+module.exports = { validateUserInput, validateUsername, validatePassword };
