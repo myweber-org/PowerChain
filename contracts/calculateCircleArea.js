b4e7681ebab526c201aa2727e5c3b919fd3048a1
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-17 23:19:18 +0000

    Add function to calculate area of a circle given its radius

--- contracts/calculateCircleArea.js
+++ contracts/calculateCircleArea.js
@@ -19,3 +19,15 @@ function validateAndCalculate() {
         resultElement.style.color = '#e74c3c';
     }
 }
+function calculateCircleArea(radius) {
+    if (typeof radius !== 'number' || radius < 0) {
+        throw new Error('Radius must be a non-negative number');
+    }
+    return Math.PI * radius * radius;
+}
+
+function formatArea(area, precision = 2) {
+    return area.toFixed(precision);
+}
+
+module.exports = { calculateCircleArea, formatArea };
