3b373364372f3d6cbc862500b5bd09770c12e133
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-18 00:37:08 +0000

    Add input validation for username and password fields

--- ui/js/vendor/validateUserInput.js
+++ ui/js/vendor/validateUserInput.js
@@ -152,4 +152,27 @@ function validateUserInput(username, email) {
     };
 }
 
-module.exports = validateUserInput;
+module.exports = validateUserInput;function validateUserInput(username, password) {
+    const errors = [];
+
+    if (!username || username.trim().length === 0) {
+        errors.push('Username is required');
+    } else if (username.length < 3) {
+        errors.push('Username must be at least 3 characters long');
+    }
+
+    if (!password || password.trim().length === 0) {
+        errors.push('Password is required');
+    } else if (password.length < 8) {
+        errors.push('Password must be at least 8 characters long');
+    } else if (!/[A-Z]/.test(password)) {
+        errors.push('Password must contain at least one uppercase letter');
+    } else if (!/[0-9]/.test(password)) {
+        errors.push('Password must contain at least one number');
+    }
+
+    return {
+        isValid: errors.length === 0,
+        errors: errors
+    };
+}
