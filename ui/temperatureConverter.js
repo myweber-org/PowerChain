7528e9b90f2b5cb2c55249878ab4939cc3938baf
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-18 15:46:51 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and vice versa

--- ui/temperatureConverter.js
+++ ui/temperatureConverter.js
@@ -1,66 +1,4 @@
 function celsiusToFahrenheit(celsius) {
-  return (celsius * 9/5) + 32;
-}
-
-function celsiusToKelvin(celsius) {
-  return celsius + 273.15;
-}
-
-function fahrenheitToCelsius(fahrenheit) {
-  return (fahrenheit - 32) * 5/9;
-}
-
-function fahrenheitToKelvin(fahrenheit) {
-  return (fahrenheit - 32) * 5/9 + 273.15;
-}
-
-function kelvinToCelsius(kelvin) {
-  return kelvin - 273.15;
-}
-
-function kelvinToFahrenheit(kelvin) {
-  return (kelvin - 273.15) * 9/5 + 32;
-}
-
-function convertTemperature(value, fromUnit, toUnit) {
-  const units = ['C', 'F', 'K'];
-  if (!units.includes(fromUnit) || !units.includes(toUnit)) {
-    throw new Error('Invalid temperature unit');
-  }
-
-  if (fromUnit === toUnit) {
-    return value;
-  }
-
-  if (fromUnit === 'C' && toUnit === 'F') {
-    return celsiusToFahrenheit(value);
-  }
-  if (fromUnit === 'C' && toUnit === 'K') {
-    return celsiusToKelvin(value);
-  }
-  if (fromUnit === 'F' && toUnit === 'C') {
-    return fahrenheitToCelsius(value);
-  }
-  if (fromUnit === 'F' && toUnit === 'K') {
-    return fahrenheitToKelvin(value);
-  }
-  if (fromUnit === 'K' && toUnit === 'C') {
-    return kelvinToCelsius(value);
-  }
-  if (fromUnit === 'K' && toUnit === 'F') {
-    return kelvinToFahrenheit(value);
-  }
-}
-
-module.exports = {
-  celsiusToFahrenheit,
-  celsiusToKelvin,
-  fahrenheitToCelsius,
-  fahrenheitToKelvin,
-  kelvinToCelsius,
-  kelvinToFahrenheit,
-  convertTemperature
-};function celsiusToFahrenheit(celsius) {
     return (celsius * 9/5) + 32;
 }
 
@@ -78,8 +16,14 @@ function convertTemperature(value, unit) {
     }
 }
 
+function formatTemperature(value, originalUnit) {
+    const convertedUnit = originalUnit.toLowerCase() === 'c' ? 'F' : 'C';
+    return `${value.toFixed(2)}°${convertedUnit}`;
+}
+
 module.exports = {
     celsiusToFahrenheit,
     fahrenheitToCelsius,
-    convertTemperature
+    convertTemperature,
+    formatTemperature
 };
