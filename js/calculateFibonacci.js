1ec751acafb2c2d344f1223edde20eac7f8b630d
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-18 18:34:35 +0000

    Add function to compute Fibonacci sequence up to a given number

--- js/calculateFibonacci.js
+++ js/calculateFibonacci.js
@@ -63,4 +63,15 @@ module.exports = { calculateFibonacci, isFibonacciNumber };function calculateFib
     return sequence;
 }
 
-console.log(calculateFibonacci(100));
+console.log(calculateFibonacci(100));function calculateFibonacci(limit) {
+    if (limit <= 0) return [];
+    if (limit === 1) return [0];
+    
+    const sequence = [0, 1];
+    while (true) {
+        const next = sequence[sequence.length - 1] + sequence[sequence.length - 2];
+        if (next > limit) break;
+        sequence.push(next);
+    }
+    return sequence;
+}
