4f501d65ccede9018052c282e42fadcc2af9d567
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-18 21:08:14 +0000

    Add function to compute the nth Fibonacci number using memoization

--- build/calculateFibonacci.js
+++ build/calculateFibonacci.js
@@ -1,15 +1,8 @@
-function generateFibonacci(limit) {
-    const sequence = [0, 1];
-    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= limit) {
-        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
-    }
-    return sequence;
-}function calculateFibonacci(n) {
-    if (n <= 0) return [];
-    if (n === 1) return [0];
-    let fib = [0, 1];
-    for (let i = 2; i < n; i++) {
-        fib.push(fib[i - 1] + fib[i - 2]);
-    }
-    return fib;
+function fibonacci(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    return memo[n];
 }
+
+console.log(fibonacci(10));
