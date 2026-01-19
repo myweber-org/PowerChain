5fccdb74b61bf6013980279e8a7c1ffdd0bdfee2
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-19 16:03:06 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and Kelvin functions

--- ui/js/temperatureConverter.js
+++ ui/js/temperatureConverter.js
@@ -2,28 +2,6 @@ function celsiusToFahrenheit(celsius) {
     return (celsius * 9/5) + 32;
 }
 
-function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-}
-
-function convertTemperature(value, unit) {
-    if (unit.toLowerCase() === 'c') {
-        return celsiusToFahrenheit(value);
-    } else if (unit.toLowerCase() === 'f') {
-        return fahrenheitToCelsius(value);
-    } else {
-        throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
-    }
-}
-
-module.exports = {
-    celsiusToFahrenheit,
-    fahrenheitToCelsius,
-    convertTemperature
-};function celsiusToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-}
-
 function celsiusToKelvin(celsius) {
     return celsius + 273.15;
 }
@@ -50,27 +28,21 @@ function convertTemperature(value, fromUnit, toUnit) {
         throw new Error('Invalid temperature unit');
     }
 
-    if (fromUnit === toUnit) {
-        return value;
+    if (fromUnit === toUnit) return value;
+
+    if (fromUnit === 'C') {
+        if (toUnit === 'F') return celsiusToFahrenheit(value);
+        if (toUnit === 'K') return celsiusToKelvin(value);
     }
 
-    if (fromUnit === 'C' && toUnit === 'F') {
-        return celsiusToFahrenheit(value);
+    if (fromUnit === 'F') {
+        if (toUnit === 'C') return fahrenheitToCelsius(value);
+        if (toUnit === 'K') return fahrenheitToKelvin(value);
     }
-    if (fromUnit === 'C' && toUnit === 'K') {
-        return celsiusToKelvin(value);
-    }
-    if (fromUnit === 'F' && toUnit === 'C') {
-        return fahrenheitToCelsius(value);
-    }
-    if (fromUnit === 'F' && toUnit === 'K') {
-        return fahrenheitToKelvin(value);
-    }
-    if (fromUnit === 'K' && toUnit === 'C') {
-        return kelvinToCelsius(value);
-    }
-    if (fromUnit === 'K' && toUnit === 'F') {
-        return kelvinToFahrenheit(value);
+
+    if (fromUnit === 'K') {
+        if (toUnit === 'C') return kelvinToCelsius(value);
+        if (toUnit === 'F') return kelvinToFahrenheit(value);
     }
 }
 
@@ -81,27 +53,5 @@ module.exports = {
     fahrenheitToKelvin,
     kelvinToCelsius,
     kelvinToFahrenheit,
-    convertTemperature
-};function celsiusToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-}
-
-function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-}
-
-function convertTemperature(value, unit) {
-    if (unit.toLowerCase() === 'c') {
-        return celsiusToFahrenheit(value);
-    } else if (unit.toLowerCase() === 'f') {
-        return fahrenheitToCelsius(value);
-    } else {
-        throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
-    }
-}
-
-module.exports = {
-    celsiusToFahrenheit,
-    fahrenheitToCelsius,
     convertTemperature
 };
