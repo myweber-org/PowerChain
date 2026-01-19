fbdd2363093349bc8839f39500c75f4d2fdb5ea4
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-19 15:41:08 +0000

    Add input validation function to sanitize user-provided strings

--- ui/fonts/validateUserInput.js
+++ ui/fonts/validateUserInput.js
@@ -12,3 +12,43 @@ function validateUserInput(username, email) {
     
     return { valid: true, message: "Input is valid." };
 }
+function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    
+    const trimmed = input.trim();
+    const sanitized = trimmed
+        .replace(/[<>]/g, '')
+        .replace(/javascript:/gi, '')
+        .replace(/on\w+=/gi, '')
+        .replace(/\s+/g, ' ');
+    
+    return sanitized.substring(0, 255);
+}
+
+function validateEmail(email) {
+    const sanitizedEmail = sanitizeInput(email);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(sanitizedEmail);
+}
+
+function validatePassword(password) {
+    const sanitizedPassword = sanitizeInput(password);
+    if (sanitizedPassword.length < 8) {
+        return false;
+    }
+    
+    const hasUpperCase = /[A-Z]/.test(sanitizedPassword);
+    const hasLowerCase = /[a-z]/.test(sanitizedPassword);
+    const hasNumbers = /\d/.test(sanitizedPassword);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(sanitizedPassword);
+    
+    return hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar;
+}
+
+module.exports = {
+    sanitizeInput,
+    validateEmail,
+    validatePassword
+};
