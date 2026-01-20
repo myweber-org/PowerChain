d8e2f80da333ac67f9bcb38e0b4bb023f30d065e
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-20 22:21:48 +0000

    Add function to calculate Fibonacci sequence up to n terms

--- ui/fonts/calculateFibonacci.js
+++ ui/fonts/calculateFibonacci.js
@@ -2,9 +2,9 @@ function calculateFibonacci(n) {
     if (n <= 0) return [];
     if (n === 1) return [0];
     
-    const fib = [0, 1];
+    const sequence = [0, 1];
     for (let i = 2; i < n; i++) {
-        fib.push(fib[i - 1] + fib[i - 2]);
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
     }
-    return fib;
+    return sequence;
 }
