95e96b2148304965b95fb4de836dacd7d3c20034
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-24 23:57:55 +0000

    Add input validation function to sanitize user-provided strings

--- ui/css/validateUserInput.js
+++ ui/css/validateUserInput.js
@@ -94,4 +94,37 @@ export { validateUserInput, validateUsername, validatePassword };function valida
     }
     
     return { username, email, isValid: true };
+}function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        throw new TypeError('Input must be a string');
+    }
+    
+    return input
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#x27;')
+        .replace(/\//g, '&#x2F;')
+        .trim();
 }
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    if (password.length < 8) {
+        return false;
+    }
+    
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    
+    return hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar;
+}
+
+export { sanitizeInput, validateEmail, validatePassword };
