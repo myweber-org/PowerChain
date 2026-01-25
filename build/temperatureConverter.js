d47339220d17c15e128fbe5b1759b7579df1ea23
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-25 19:23:14 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and Kelvin functions

--- build/temperatureConverter.js
+++ build/temperatureConverter.js
@@ -2,24 +2,61 @@ function celsiusToFahrenheit(celsius) {
     return (celsius * 9/5) + 32;
 }
 
+function celsiusToKelvin(celsius) {
+    return celsius + 273.15;
+}
+
 function fahrenheitToCelsius(fahrenheit) {
     return (fahrenheit - 32) * 5/9;
 }
 
+function fahrenheitToKelvin(fahrenheit) {
+    return (fahrenheit - 32) * 5/9 + 273.15;
+}
+
+function kelvinToCelsius(kelvin) {
+    return kelvin - 273.15;
+}
+
+function kelvinToFahrenheit(kelvin) {
+    return (kelvin - 273.15) * 9/5 + 32;
+}
+
 function convertTemperature(value, fromUnit, toUnit) {
-    if (fromUnit === 'C' && toUnit === 'F') {
-        return celsiusToFahrenheit(value);
-    } else if (fromUnit === 'F' && toUnit === 'C') {
-        return fahrenheitToCelsius(value);
-    } else if (fromUnit === toUnit) {
-        return value;
-    } else {
-        throw new Error('Unsupported temperature conversion');
+    const units = ['C', 'F', 'K'];
+    if (!units.includes(fromUnit) || !units.includes(toUnit)) {
+        throw new Error('Invalid temperature unit');
     }
+    
+    if (fromUnit === toUnit) {
+        return value;
+    }
+    
+    const conversionMap = {
+        'C_F': celsiusToFahrenheit,
+        'C_K': celsiusToKelvin,
+        'F_C': fahrenheitToCelsius,
+        'F_K': fahrenheitToKelvin,
+        'K_C': kelvinToCelsius,
+        'K_F': kelvinToFahrenheit
+    };
+    
+    const conversionKey = `${fromUnit}_${toUnit}`;
+    const conversionFunction = conversionMap[conversionKey];
+    
+    if (conversionFunction) {
+        return conversionFunction(value);
+    }
+    
+    throw new Error('Unsupported temperature conversion');
 }
 
 module.exports = {
     celsiusToFahrenheit,
+    celsiusToKelvin,
     fahrenheitToCelsius,
+    fahrenheitToKelvin,
+    kelvinToCelsius,
+    kelvinToFahrenheit,
     convertTemperature
 };
