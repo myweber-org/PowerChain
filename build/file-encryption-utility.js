1e689ec33270304228557bcee77b3c2ee35c2e55
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-02 00:08:35 +0000

    Add file encryption utility with AES-256-CBC implementation

--- build/file-encryption-utility.js
+++ build/file-encryption-utility.js
@@ -1,74 +1,76 @@
 const crypto = require('crypto');
 
 class FileEncryptionUtility {
-  constructor() {
-    this.algorithm = 'aes-256-gcm';
-    this.keyLength = 32;
-    this.ivLength = 16;
-    this.authTagLength = 16;
-  }
-
-  generateKey() {
-    return crypto.randomBytes(this.keyLength);
-  }
-
-  encryptFile(data, key) {
-    if (!Buffer.isBuffer(data)) {
-      data = Buffer.from(data);
+    constructor() {
+        this.algorithm = 'aes-256-cbc';
+        this.keyLength = 32;
+        this.ivLength = 16;
     }
 
-    const iv = crypto.randomBytes(this.ivLength);
-    const cipher = crypto.createCipheriv(this.algorithm, key, iv);
-    
-    const encrypted = Buffer.concat([
-      cipher.update(data),
-      cipher.final()
-    ]);
-    
-    const authTag = cipher.getAuthTag();
-    
-    return Buffer.concat([iv, authTag, encrypted]);
-  }
-
-  decryptFile(encryptedData, key) {
-    if (!Buffer.isBuffer(encryptedData)) {
-      throw new Error('Encrypted data must be a Buffer');
+    generateKey() {
+        return crypto.randomBytes(this.keyLength);
     }
 
-    const iv = encryptedData.slice(0, this.ivLength);
-    const authTag = encryptedData.slice(this.ivLength, this.ivLength + this.authTagLength);
-    const encryptedContent = encryptedData.slice(this.ivLength + this.authTagLength);
+    generateIV() {
+        return crypto.randomBytes(this.ivLength);
+    }
 
-    const decipher = crypto.createDecipheriv(this.algorithm, key, iv);
-    decipher.setAuthTag(authTag);
+    encryptFile(data, key, iv) {
+        if (!Buffer.isBuffer(data)) {
+            data = Buffer.from(data);
+        }
 
-    return Buffer.concat([
-      decipher.update(encryptedContent),
-      decipher.final()
-    ]);
-  }
+        const cipher = crypto.createCipheriv(this.algorithm, key, iv);
+        const encrypted = Buffer.concat([cipher.update(data), cipher.final()]);
+        
+        return {
+            encryptedData: encrypted,
+            iv: iv,
+            key: key
+        };
+    }
 
-  async encryptFileAsync(data, key) {
-    return new Promise((resolve, reject) => {
-      try {
-        const result = this.encryptFile(data, key);
-        resolve(result);
-      } catch (error) {
-        reject(error);
-      }
-    });
-  }
+    decryptFile(encryptedData, key, iv) {
+        const decipher = crypto.createDecipheriv(this.algorithm, key, iv);
+        const decrypted = Buffer.concat([decipher.update(encryptedData), decipher.final()]);
+        
+        return decrypted;
+    }
 
-  async decryptFileAsync(encryptedData, key) {
-    return new Promise((resolve, reject) => {
-      try {
-        const result = this.decryptFile(encryptedData, key);
-        resolve(result);
-      } catch (error) {
-        reject(error);
-      }
-    });
-  }
+    encryptFileWithPassword(data, password) {
+        const salt = crypto.randomBytes(16);
+        const key = crypto.pbkdf2Sync(password, salt, 100000, this.keyLength, 'sha256');
+        const iv = this.generateIV();
+        
+        const cipher = crypto.createCipheriv(this.algorithm, key, iv);
+        const encrypted = Buffer.concat([cipher.update(data), cipher.final()]);
+        
+        return {
+            encryptedData: encrypted,
+            iv: iv,
+            salt: salt
+        };
+    }
+
+    decryptFileWithPassword(encryptedData, password, salt, iv) {
+        const key = crypto.pbkdf2Sync(password, salt, 100000, this.keyLength, 'sha256');
+        const decipher = crypto.createDecipheriv(this.algorithm, key, iv);
+        const decrypted = Buffer.concat([decipher.update(encryptedData), decipher.final()]);
+        
+        return decrypted;
+    }
+
+    hashFile(data) {
+        return crypto.createHash('sha256').update(data).digest('hex');
+    }
+
+    verifyFileHash(data, expectedHash) {
+        const actualHash = this.hashFile(data);
+        return crypto.timingSafeEqual(
+            Buffer.from(actualHash, 'hex'),
+            Buffer.from(expectedHash, 'hex')
+        );
+    }
 }
 
 module.exports = FileEncryptionUtility;
