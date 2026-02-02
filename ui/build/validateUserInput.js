f944cece905256ad9ef818ea09f887f422ee49ec
InkspillVerse <inkspill.verse@outlook.com>
2026-02-02 00:52:09 +0000

    Add input validation function to sanitize user-provided strings

--- ui/build/validateUserInput.js
+++ ui/build/validateUserInput.js
@@ -25,4 +25,40 @@ function validateUserInput(username, email) {
   };
 }
 
-module.exports = validateUserInput;
+module.exports = validateUserInput;function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    return input
+        .replace(/[&<>"']/g, function(match) {
+            const escapeMap = {
+                '&': '&amp;',
+                '<': '&lt;',
+                '>': '&gt;',
+                '"': '&quot;',
+                "'": '&#x27;'
+            };
+            return escapeMap[match] || match;
+        })
+        .trim()
+        .substring(0, 255);
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(sanitizeInput(email));
+}
+
+function validatePassword(password) {
+    const sanitizedPassword = sanitizeInput(password);
+    return sanitizedPassword.length >= 8 && 
+           /[A-Z]/.test(sanitizedPassword) && 
+           /[a-z]/.test(sanitizedPassword) && 
+           /\d/.test(sanitizedPassword);
+}
+
+module.exports = {
+    sanitizeInput,
+    validateEmail,
+    validatePassword
+};
