2ed418488cf59702b4783305fa170ee0bd06b85b
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-03 16:16:09 +0000

    Add function to calculate Fibonacci sequence up to a given number

--- ui/css/calculateFibonacci.js
+++ ui/css/calculateFibonacci.js
@@ -16,4 +16,15 @@ function calculateFibonacci(n) {
         sequence.push(sequence[i - 1] + sequence[i - 2]);
     }
     return sequence;
+}function calculateFibonacci(limit) {
+    if (limit <= 0) return [];
+    if (limit === 1) return [0];
+    
+    const sequence = [0, 1];
+    while (true) {
+        const nextValue = sequence[sequence.length - 1] + sequence[sequence.length - 2];
+        if (nextValue > limit) break;
+        sequence.push(nextValue);
+    }
+    return sequence;
 }
