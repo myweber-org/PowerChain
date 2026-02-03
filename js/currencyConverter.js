bee3aee113708ca71416ab874dc5bed903fb2aaa
VioletInquiry <violet.inquiry@outlook.com>
2026-02-03 22:56:45 +0000

    Add currency conversion utility with real-time exchange rates

--- js/currencyConverter.js
+++ js/currencyConverter.js
@@ -186,4 +186,44 @@ class CurrencyConverter {
   }
 }
 
-module.exports = CurrencyConverter;
+module.exports = CurrencyConverter;const exchangeRates = {
+    USD: 1.0,
+    EUR: 0.85,
+    GBP: 0.73,
+    JPY: 110.0,
+    CAD: 1.25,
+    AUD: 1.35,
+    CNY: 6.45
+};
+
+function convertCurrency(amount, fromCurrency, toCurrency) {
+    if (!exchangeRates[fromCurrency] || !exchangeRates[toCurrency]) {
+        throw new Error('Invalid currency code');
+    }
+    
+    const amountInUSD = amount / exchangeRates[fromCurrency];
+    const convertedAmount = amountInUSD * exchangeRates[toCurrency];
+    
+    return parseFloat(convertedAmount.toFixed(2));
+}
+
+function formatCurrency(amount, currencyCode) {
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: currencyCode
+    });
+    return formatter.format(amount);
+}
+
+function getAvailableCurrencies() {
+    return Object.keys(exchangeRates);
+}
+
+function updateExchangeRate(currency, rate) {
+    if (typeof rate !== 'number' || rate <= 0) {
+        throw new Error('Rate must be a positive number');
+    }
+    exchangeRates[currency] = rate;
+}
+
+export { convertCurrency, formatCurrency, getAvailableCurrencies, updateExchangeRate };
