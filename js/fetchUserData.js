4ef68d932f53ca24425e17443cc638c1f67631e1
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-04 00:20:43 +0000

    Add fetchUserData function to retrieve and display user information from API

--- js/fetchUserData.js
+++ js/fetchUserData.js
@@ -169,4 +169,25 @@ function displayUserData(user) {
     }
 }
 
+fetchUserData(1);function fetchUserData(userId) {
+    const apiUrl = `https://jsonplaceholder.typicode.com/users/${userId}`;
+    
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(`User ID: ${data.id}`);
+            console.log(`Name: ${data.name}`);
+            console.log(`Email: ${data.email}`);
+            console.log(`Company: ${data.company.name}`);
+        })
+        .catch(error => {
+            console.error('Error fetching user data:', error);
+        });
+}
+
 fetchUserData(1);
