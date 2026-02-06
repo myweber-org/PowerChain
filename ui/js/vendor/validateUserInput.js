5073d12804d829e464b57502e978902c6ada23e4
ElaraWrites <elara.writes@outlook.com>
2026-02-06 02:58:08 +0000

    Add input validation function to sanitize user-provided strings

--- ui/js/vendor/validateUserInput.js
+++ ui/js/vendor/validateUserInput.js
@@ -98,4 +98,46 @@ export { validateUsername, validateEmail, validateUserInput };function validateU
     };
 }
 
-module.exports = validateUserInput;
+module.exports = validateUserInput;function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    return input
+        .replace(/[<>&"']/g, function(match) {
+            const escapeMap = {
+                '<': '&lt;',
+                '>': '&gt;',
+                '&': '&amp;',
+                '"': '&quot;',
+                "'": '&#x27;'
+            };
+            return escapeMap[match] || match;
+        })
+        .trim()
+        .substring(0, 255);
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    
+    return password.length >= minLength && 
+           hasUpperCase && 
+           hasLowerCase && 
+           hasNumbers && 
+           hasSpecialChar;
+}
+
+module.exports = {
+    sanitizeInput,
+    validateEmail,
+    validatePassword
+};
