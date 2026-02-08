332aca992d9bbc1fabd890937e0eef628279547c
InkspillVerse <inkspill.verse@outlook.com>
2026-02-08 00:22:13 +0000

    Add password strength validation utility

--- ui/js/vendor/passwordValidator.js
+++ ui/js/vendor/passwordValidator.js
@@ -54,4 +54,28 @@ function generateSecurePassword(length = 12) {
     return password;
 }
 
-export { validatePasswordStrength, generateSecurePassword };
+export { validatePasswordStrength, generateSecurePassword };function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    
+    if (password.length < minLength) {
+        return { valid: false, message: "Password must be at least 8 characters long" };
+    }
+    if (!hasUpperCase) {
+        return { valid: false, message: "Password must contain at least one uppercase letter" };
+    }
+    if (!hasLowerCase) {
+        return { valid: false, message: "Password must contain at least one lowercase letter" };
+    }
+    if (!hasNumbers) {
+        return { valid: false, message: "Password must contain at least one number" };
+    }
+    if (!hasSpecialChar) {
+        return { valid: false, message: "Password must contain at least one special character" };
+    }
+    
+    return { valid: true, message: "Password is strong" };
+}
