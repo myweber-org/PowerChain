a888ae97ea55553a9264806529c695aa35bed858
FelixMakes <felix.makes@hotmail.com>
2026-02-09 21:51:23 +0000

    Add data validation utility for user input sanitization

--- build/dataValidator.js
+++ build/dataValidator.js
@@ -130,4 +130,26 @@ function validateFormData(data) {
     };
 }
 
-export { validateEmail, validatePhoneNumber, validateFormData };
+export { validateEmail, validatePhoneNumber, validateFormData };function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    return password.length >= 8 && 
+           /[A-Z]/.test(password) && 
+           /[a-z]/.test(password) && 
+           /\d/.test(password);
+}
+
+function sanitizeInput(input) {
+    return input.trim()
+                .replace(/[<>]/g, '')
+                .substring(0, 255);
+}
+
+module.exports = {
+    validateEmail,
+    validatePassword,
+    sanitizeInput
+};
