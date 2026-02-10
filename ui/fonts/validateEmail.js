602c43c9a1a9a54335f57431fb293c11e61fbf09
ElaraWrites <elara.writes@outlook.com>
2026-02-10 22:04:52 +0000

    Add email validation utility with regex pattern

--- ui/fonts/validateEmail.js
+++ ui/fonts/validateEmail.js
@@ -9,4 +9,9 @@ function validateEmail(email) {
     return emailRegex.test(email);
 }
 
+module.exports = validateEmail;function validateEmail(email) {
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return pattern.test(email);
+}
+
 module.exports = validateEmail;
