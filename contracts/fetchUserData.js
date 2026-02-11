4d9a4f76df6ca802c1012b38425d05738254199a
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-11 01:28:30 +0000

    Add function to fetch and display user data from API

--- contracts/fetchUserData.js
+++ contracts/fetchUserData.js
@@ -90,4 +90,20 @@ function clearUserCache(userId = null) {
     }
 }
 
-export { fetchUserData, clearUserCache };
+export { fetchUserData, clearUserCache };function fetchUserData(userId) {
+  return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('User data:', data);
+      return data;
+    })
+    .catch(error => {
+      console.error('Error fetching user data:', error);
+      throw error;
+    });
+}
