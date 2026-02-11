948255a0a8c39f3ff576ee5463613f158b081be2
InkspillVerse <inkspill.verse@outlook.com>
2026-02-11 00:01:23 +0000

    Add input validation function for username and password

--- contracts/validateUserInput.js
+++ contracts/validateUserInput.js
@@ -232,4 +232,33 @@ export { validateUserInput, validateUsername, validateEmail };function validateU
         valid: true,
         message: 'Input validation successful.'
     };
+}function validateUsername(username) {
+  const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+  return usernameRegex.test(username);
 }
+
+function validatePassword(password) {
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/;
+  return passwordRegex.test(password);
+}
+
+function validateUserInput(username, password) {
+  const usernameValid = validateUsername(username);
+  const passwordValid = validatePassword(password);
+  
+  if (!usernameValid && !passwordValid) {
+    return 'Username and password are invalid';
+  } else if (!usernameValid) {
+    return 'Username is invalid';
+  } else if (!passwordValid) {
+    return 'Password is invalid';
+  } else {
+    return 'User input is valid';
+  }
+}
+
+module.exports = {
+  validateUsername,
+  validatePassword,
+  validateUserInput
+};
