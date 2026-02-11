efbfe1a8b959f17445192d287a905e1a91733234
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-11 18:58:36 +0000

    Add function to calculate Fibonacci sequence up to a given number

--- contracts/calculateFibonacci.js
+++ contracts/calculateFibonacci.js
@@ -1,51 +1,10 @@
-function calculateFibonacci(limit) {
-    if (limit <= 0) return [];
-    if (limit === 1) return [0];
-    
-    const sequence = [0, 1];
-    while (true) {
-        const next = sequence[sequence.length - 1] + sequence[sequence.length - 2];
-        if (next > limit) break;
-        sequence.push(next);
-    }
-    return sequence;
-}
-function calculateFibonacci(n, memo = {}) {
-    if (n in memo) return memo[n];
-    if (n <= 1) return n;
-    memo[n] = calculateFibonacci(n - 1, memo) + calculateFibonacci(n - 2, memo);
-    return memo[n];
-}function calculateFibonacci(n) {
+function calculateFibonacci(n) {
     if (n <= 0) return [];
     if (n === 1) return [0];
     
-    let fib = [0, 1];
-    for (let i = 2; i < n; i++) {
-        fib.push(fib[i - 1] + fib[i - 2]);
-    }
-    return fib;
-}
-
-function displayFibonacci() {
-    const n = 10;
-    const result = calculateFibonacci(n);
-    console.log(`First ${n} Fibonacci numbers:`, result);
-}
-
-displayFibonacci();function calculateFibonacci(n) {
-    if (n <= 0) return [];
-    if (n === 1) return [0];
-    const sequence = [0, 1];
-    for (let i = 2; i < n; i++) {
-        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    let sequence = [0, 1];
+    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= n) {
+        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
     }
     return sequence;
 }
-function fibonacci(n, memo = {}) {
-    if (n in memo) return memo[n];
-    if (n <= 2) return 1;
-    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
-    return memo[n];
-}
-
-module.exports = fibonacci;
