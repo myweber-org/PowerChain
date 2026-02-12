940ddb40956e6f39470e75611b4006b3ba1f58a9
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-12 23:08:52 +0000

    Add function to calculate Fibonacci sequence up to n terms

--- contracts/calculateFibonacci.js
+++ contracts/calculateFibonacci.js
@@ -7,4 +7,21 @@ function calculateFibonacci(n) {
         sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
     }
     return sequence;
+}function calculateFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    
+    let fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+        fib.push(fib[i-1] + fib[i-2]);
+    }
+    return fib;
 }
+
+function displayFibonacci(limit) {
+    const result = calculateFibonacci(limit);
+    console.log(`Fibonacci sequence (${limit} terms):`, result);
+    return result;
+}
+
+module.exports = { calculateFibonacci, displayFibonacci };
