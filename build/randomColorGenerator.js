26b46faa73d090cdbae40aa4a842c8fd5093e9ad
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-14 20:08:30 +0000

    Add random color generator utility function

--- build/randomColorGenerator.js
+++ build/randomColorGenerator.js
@@ -40,4 +40,21 @@ function displayRandomColor() {
     return color;
 }
 
-module.exports = { generateRandomColor, displayRandomColor };
+module.exports = { generateRandomColor, displayRandomColor };function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function generateRandomColors(count) {
+    const colors = [];
+    for (let i = 0; i < count; i++) {
+        colors.push(getRandomColor());
+    }
+    return colors;
+}
+
+export { getRandomColor, generateRandomColors };
