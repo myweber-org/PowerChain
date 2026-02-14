cb311b1e6ed0204f77d727557b54560b550d0b1d
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-14 20:29:33 +0000

    Add user preferences management with localStorage persistence

--- ui/js/vendor/userPreferencesManager.js
+++ ui/js/vendor/userPreferencesManager.js
@@ -128,4 +128,50 @@ const userPreferencesManager = (() => {
 
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = UserPreferencesManager;
-}
+}const userPreferencesManager = (() => {
+    const STORAGE_KEY = 'app_preferences';
+    const defaultPreferences = {
+        theme: 'light',
+        fontSize: 16,
+        notifications: true,
+        language: 'en'
+    };
+
+    function getPreferences() {
+        const stored = localStorage.getItem(STORAGE_KEY);
+        return stored ? JSON.parse(stored) : { ...defaultPreferences };
+    }
+
+    function updatePreferences(newPreferences) {
+        const current = getPreferences();
+        const updated = { ...current, ...newPreferences };
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(updated));
+        dispatchChangeEvent(updated);
+        return updated;
+    }
+
+    function resetPreferences() {
+        localStorage.removeItem(STORAGE_KEY);
+        dispatchChangeEvent(defaultPreferences);
+        return { ...defaultPreferences };
+    }
+
+    function dispatchChangeEvent(preferences) {
+        const event = new CustomEvent('preferencesChanged', {
+            detail: preferences
+        });
+        window.dispatchEvent(event);
+    }
+
+    function getPreference(key) {
+        const prefs = getPreferences();
+        return prefs[key] !== undefined ? prefs[key] : defaultPreferences[key];
+    }
+
+    return {
+        get: getPreference,
+        getAll: getPreferences,
+        update: updatePreferences,
+        reset: resetPreferences
+    };
+})();
