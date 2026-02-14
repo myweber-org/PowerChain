a2afef76d5f251d7fd07722bc743307a00022aa0
VioletInquiry <violet.inquiry@outlook.com>
2026-02-14 23:44:22 +0000

    Add function to calculate Fibonacci sequence up to a given number

--- ui/js/calculateFibonacci.js
+++ ui/js/calculateFibonacci.js
@@ -37,4 +37,28 @@ module.exports = { calculateFibonacci, displayFibonacci };function calculateFibo
     if (n <= 2) return 1;
     memo[n] = calculateFibonacci(n - 1, memo) + calculateFibonacci(n - 2, memo);
     return memo[n];
+}function calculateFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    
+    const sequence = [0, 1];
+    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= n) {
+        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+    }
+    return sequence;
 }
+
+function isFibonacciNumber(num) {
+    if (num < 0) return false;
+    if (num === 0 || num === 1) return true;
+    
+    let a = 0, b = 1;
+    while (b < num) {
+        const temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b === num;
+}
+
+module.exports = { calculateFibonacci, isFibonacciNumber };
