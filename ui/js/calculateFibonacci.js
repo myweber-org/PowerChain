2c1b5066dc61435910e3959cdac25627d2ea4884
InkspillVerse <inkspill.verse@outlook.com>
2026-02-15 22:35:22 +0000

    Add function to calculate the nth Fibonacci number using memoization for efficiency

--- ui/js/calculateFibonacci.js
+++ ui/js/calculateFibonacci.js
@@ -61,4 +61,9 @@ function isFibonacciNumber(num) {
     return b === num;
 }
 
-module.exports = { calculateFibonacci, isFibonacciNumber };
+module.exports = { calculateFibonacci, isFibonacciNumber };function calculateFibonacci(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = calculateFibonacci(n - 1, memo) + calculateFibonacci(n - 2, memo);
+    return memo[n];
+}
