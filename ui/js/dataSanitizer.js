4a30454182f8c0ba8388d27d2d0da1b465983a03
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-15 00:25:03 +0000

    Add data sanitization utility to prevent XSS attacks

--- ui/js/dataSanitizer.js
+++ ui/js/dataSanitizer.js
@@ -20,4 +20,25 @@ function escapeHtml(text) {
   return text.replace(/[&<>"']/g, function(m) { return map[m]; });
 }
 
-module.exports = { sanitizeInput, validateEmail, escapeHtml };
+module.exports = { sanitizeInput, validateEmail, escapeHtml };function sanitizeInput(input) {
+  if (typeof input !== 'string') {
+    return '';
+  }
+  const map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#x27;',
+    '/': '&#x2F;'
+  };
+  const reg = /[&<>"'/]/ig;
+  return input.replace(reg, (match) => map[match]);
+}
+
+function validateEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+}
+
+module.exports = { sanitizeInput, validateEmail };
