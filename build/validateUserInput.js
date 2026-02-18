253572dc706e5ffaa5e644e8201fda7e6843b3c6
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-18 14:55:26 +0000

    Add input validation function to sanitize user-provided strings

--- build/validateUserInput.js
+++ build/validateUserInput.js
@@ -113,4 +113,34 @@ module.exports = { sanitizeInput, validateEmail, validatePassword };function val
     }
     
     return { username, email, isValid: true };
+}function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    return input.trim()
+        .replace(/[<>]/g, '')
+        .replace(/javascript:/gi, '')
+        .substring(0, 255);
 }
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    if (password.length < 8) {
+        return false;
+    }
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    return hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar;
+}
+
+module.exports = {
+    sanitizeInput,
+    validateEmail,
+    validatePassword
+};
