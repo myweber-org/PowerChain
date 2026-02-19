3bfed092ffc7330d38b1bba16c3872ed19a7a4ad
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-19 14:17:47 +0000

    Add input validation for username and password fields

--- ui/js/validateUserInput.js
+++ ui/js/validateUserInput.js
@@ -69,4 +69,33 @@ export { validateUserInput, validateUsername, validateEmail };function validateU
         isValid: errors.length === 0,
         errors: errors
     };
+}function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
 }
+
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+    
+    if (!usernameValid && !passwordValid) {
+        return 'Invalid username and password';
+    } else if (!usernameValid) {
+        return 'Invalid username';
+    } else if (!passwordValid) {
+        return 'Invalid password';
+    } else {
+        return 'Valid input';
+    }
+}
+
+module.exports = {
+    validateUsername,
+    validatePassword,
+    validateUserInput
+};
