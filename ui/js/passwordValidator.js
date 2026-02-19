736c8e93d454277f33dd55f04afd0f77e2703bd0
heyQuim <hey.quim@hotmail.com>
2026-02-19 00:05:49 +0000

    Add password strength validation utility with regex patterns

--- ui/js/passwordValidator.js
+++ ui/js/passwordValidator.js
@@ -179,4 +179,48 @@ function calculatePasswordStrength(password) {
     return Math.min(score, 7);
 }
 
-export { validatePassword, calculatePasswordStrength };
+export { validatePassword, calculatePasswordStrength };function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    
+    if (password.length < minLength) {
+        return { valid: false, message: 'Password must be at least 8 characters long' };
+    }
+    
+    if (!hasUpperCase) {
+        return { valid: false, message: 'Password must contain at least one uppercase letter' };
+    }
+    
+    if (!hasLowerCase) {
+        return { valid: false, message: 'Password must contain at least one lowercase letter' };
+    }
+    
+    if (!hasNumbers) {
+        return { valid: false, message: 'Password must contain at least one number' };
+    }
+    
+    if (!hasSpecialChar) {
+        return { valid: false, message: 'Password must contain at least one special character' };
+    }
+    
+    return { valid: true, message: 'Password meets all requirements' };
+}
+
+function calculatePasswordStrength(password) {
+    let strength = 0;
+    
+    if (password.length >= 12) strength += 2;
+    else if (password.length >= 8) strength += 1;
+    
+    if (/[A-Z]/.test(password)) strength += 1;
+    if (/[a-z]/.test(password)) strength += 1;
+    if (/\d/.test(password)) strength += 1;
+    if (/[^A-Za-z0-9]/.test(password)) strength += 1;
+    
+    return strength;
+}
+
+module.exports = { validatePassword, calculatePasswordStrength };
