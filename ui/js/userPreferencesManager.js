d63cfb924df11355730cdd1eb6141a7f884b0dbd
heyQuim <hey.quim@hotmail.com>
2026-02-20 22:38:59 +0000

    Add user preferences management with localStorage support

--- ui/js/userPreferencesManager.js
+++ ui/js/userPreferencesManager.js
@@ -238,4 +238,59 @@ const userPrefs = new UserPreferencesManager();const UserPreferencesManager = (f
         clear: clearAllPreferences,
         getAll: getAllPreferences
     };
+})();const userPreferencesManager = (function() {
+    const PREFERENCES_KEY = 'app_preferences';
+    
+    function getPreferences() {
+        const stored = localStorage.getItem(PREFERENCES_KEY);
+        return stored ? JSON.parse(stored) : {};
+    }
+    
+    function setPreference(key, value) {
+        const preferences = getPreferences();
+        preferences[key] = value;
+        localStorage.setItem(PREFERENCES_KEY, JSON.stringify(preferences));
+        return preferences;
+    }
+    
+    function removePreference(key) {
+        const preferences = getPreferences();
+        delete preferences[key];
+        localStorage.setItem(PREFERENCES_KEY, JSON.stringify(preferences));
+        return preferences;
+    }
+    
+    function clearPreferences() {
+        localStorage.removeItem(PREFERENCES_KEY);
+        return {};
+    }
+    
+    function hasPreference(key) {
+        const preferences = getPreferences();
+        return key in preferences;
+    }
+    
+    function getAllKeys() {
+        const preferences = getPreferences();
+        return Object.keys(preferences);
+    }
+    
+    function getPreference(key, defaultValue = null) {
+        const preferences = getPreferences();
+        return preferences[key] !== undefined ? preferences[key] : defaultValue;
+    }
+    
+    return {
+        get: getPreference,
+        set: setPreference,
+        remove: removePreference,
+        clear: clearPreferences,
+        has: hasPreference,
+        keys: getAllKeys,
+        getAll: getPreferences
+    };
 })();
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = userPreferencesManager;
+}
