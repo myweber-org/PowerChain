c60a28fa6bf3d1ed71ac58431cd12de8000e3c6b
ElaraWrites <elara.writes@outlook.com>
2026-02-22 01:16:15 +0000

    Add utility function to sort files by extension and size

--- ui/fileSorter.js
+++ ui/fileSorter.js
@@ -7,4 +7,13 @@ function sortFiles(files) {
         }
         return a.size - b.size;
     });
+}function sortFiles(files) {
+    return files.sort((a, b) => {
+        const extA = a.name.split('.').pop().toLowerCase();
+        const extB = b.name.split('.').pop().toLowerCase();
+        if (extA !== extB) {
+            return extA.localeCompare(extB);
+        }
+        return a.size - b.size;
+    });
 }
