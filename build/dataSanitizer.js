7b7b16055f89b488c2ecc50e42a7dca0ed642a37
ElaraWrites <elara.writes@outlook.com>
2026-02-22 21:16:21 +0000

    Add data sanitization utility to prevent XSS attacks

--- build/dataSanitizer.js
+++ build/dataSanitizer.js
@@ -72,4 +72,26 @@ function escapeHtml(unsafe) {
         .replace(/'/g, "&#039;");
 }
 
+export { sanitizeInput, validateEmail, escapeHtml };function sanitizeInput(input) {
+  const div = document.createElement('div');
+  div.textContent = input;
+  return div.innerHTML;
+}
+
+function validateEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+}
+
+function escapeHtml(text) {
+  const map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#039;'
+  };
+  return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+
 export { sanitizeInput, validateEmail, escapeHtml };
