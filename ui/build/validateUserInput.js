87b354c3823978f7c145669dfa6ba18eebf2263d
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-22 15:32:03 +0000

    Add input validation function for username and password

--- ui/build/validateUserInput.js
+++ ui/build/validateUserInput.js
@@ -134,4 +134,31 @@ function validateUserInput(username, password) {
     return { valid: true, message: "User input is valid" };
 }
 
-export { validateUserInput, validateUsername, validatePassword };
+export { validateUserInput, validateUsername, validatePassword };function validateUserInput(username, password) {
+  const errors = [];
+
+  if (!username || username.trim() === '') {
+    errors.push('Username is required');
+  } else if (username.length < 3) {
+    errors.push('Username must be at least 3 characters long');
+  } else if (username.length > 20) {
+    errors.push('Username must be less than 20 characters');
+  }
+
+  if (!password || password.trim() === '') {
+    errors.push('Password is required');
+  } else if (password.length < 8) {
+    errors.push('Password must be at least 8 characters long');
+  } else if (!/[A-Z]/.test(password)) {
+    errors.push('Password must contain at least one uppercase letter');
+  } else if (!/[0-9]/.test(password)) {
+    errors.push('Password must contain at least one number');
+  } else if (!/[!@#$%^&*]/.test(password)) {
+    errors.push('Password must contain at least one special character');
+  }
+
+  return {
+    isValid: errors.length === 0,
+    errors: errors
+  };
+}
