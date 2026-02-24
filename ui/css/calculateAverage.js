361db021b952c65cb73bcd7ec93cc867f6e5a099
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-24 19:04:08 +0000

    Add function to calculate average of an array of numbers

--- ui/css/calculateAverage.js
+++ ui/css/calculateAverage.js
@@ -18,4 +18,10 @@ module.exports = calculateAverage;function calculateAverage(numbers) {
     }, 0);
     
     return sum / numbers.length;
+}function calculateAverage(numbers) {
+    if (!Array.isArray(numbers) || numbers.length === 0) {
+        return 0;
+    }
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
+    return sum / numbers.length;
 }
