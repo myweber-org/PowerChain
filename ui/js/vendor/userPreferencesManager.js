7e0b4f531cb9dd78d6bb0c93e4a5180b6af9c0c1
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-24 15:54:59 +0000

    Add user preferences management with localStorage persistence

--- ui/js/vendor/userPreferencesManager.js
+++ ui/js/vendor/userPreferencesManager.js
@@ -186,4 +186,63 @@ const UserPreferencesManager = (() => {
   };
 })();
 
-export default UserPreferencesManager;
+export default UserPreferencesManager;class UserPreferencesManager {
+    constructor() {
+        this.preferences = this.loadPreferences();
+    }
+
+    loadPreferences() {
+        const stored = localStorage.getItem('userPreferences');
+        return stored ? JSON.parse(stored) : {
+            theme: 'light',
+            language: 'en',
+            notifications: true,
+            fontSize: 16
+        };
+    }
+
+    savePreferences() {
+        localStorage.setItem('userPreferences', JSON.stringify(this.preferences));
+    }
+
+    setPreference(key, value) {
+        if (this.preferences.hasOwnProperty(key)) {
+            this.preferences[key] = value;
+            this.savePreferences();
+            return true;
+        }
+        return false;
+    }
+
+    getPreference(key) {
+        return this.preferences[key];
+    }
+
+    resetPreferences() {
+        this.preferences = {
+            theme: 'light',
+            language: 'en',
+            notifications: true,
+            fontSize: 16
+        };
+        this.savePreferences();
+    }
+
+    exportPreferences() {
+        return JSON.stringify(this.preferences, null, 2);
+    }
+
+    importPreferences(jsonString) {
+        try {
+            const imported = JSON.parse(jsonString);
+            this.preferences = { ...this.preferences, ...imported };
+            this.savePreferences();
+            return true;
+        } catch (error) {
+            console.error('Invalid preferences format:', error);
+            return false;
+        }
+    }
+}
+
+const preferenceManager = new UserPreferencesManager();
