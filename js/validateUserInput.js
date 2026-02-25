bba7b2efac165f5c235873b4054348ff66051385
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-25 01:28:07 +0000

    Add input validation function for username and password

--- js/validateUserInput.js
+++ js/validateUserInput.js
@@ -41,4 +41,35 @@ function validateUserInput(username, password) {
     return { username, password };
 }
 
+module.exports = { validateUserInput, validateUsername, validatePassword };function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    if (password.length < 8) {
+        return false;
+    }
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    return hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar;
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+    
+    if (!usernameValid && !passwordValid) {
+        return { valid: false, message: "Invalid username and password" };
+    } else if (!usernameValid) {
+        return { valid: false, message: "Invalid username" };
+    } else if (!passwordValid) {
+        return { valid: false, message: "Invalid password" };
+    } else {
+        return { valid: true, message: "Validation successful" };
+    }
+}
+
 module.exports = { validateUserInput, validateUsername, validatePassword };
