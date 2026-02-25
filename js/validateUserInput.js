a2636f112059773e8e317c2680211954216896ef
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-25 23:43:55 +0000

    Add input validation function for username and password

--- js/validateUserInput.js
+++ js/validateUserInput.js
@@ -49,4 +49,33 @@ function validatePassword(password) {
            /\d/.test(password);
 }
 
-module.exports = { sanitizeInput, validateEmail, validatePassword };
+module.exports = { sanitizeInput, validateEmail, validatePassword };function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+    
+    if (!usernameValid && !passwordValid) {
+        return { valid: false, message: 'Username and password are invalid' };
+    }
+    
+    if (!usernameValid) {
+        return { valid: false, message: 'Username is invalid' };
+    }
+    
+    if (!passwordValid) {
+        return { valid: false, message: 'Password is invalid' };
+    }
+    
+    return { valid: true, message: 'User input is valid' };
+}
+
+module.exports = { validateUserInput, validateUsername, validatePassword };
