f63ea34e7e0aa1bd3f099c6489236cc427ec739c
FelixMakes <felix.makes@hotmail.com>
2026-02-26 14:50:11 +0000

    Add factorial calculation function with memoization

--- contracts/calculateFactorial.js
+++ contracts/calculateFactorial.js
@@ -14,4 +14,13 @@ function calculateFactorial(n) {
     return result;
 }
 
+module.exports = calculateFactorial;function calculateFactorial(n, memo = {}) {
+    if (n < 0) throw new Error('Factorial is not defined for negative numbers');
+    if (n === 0 || n === 1) return 1;
+    if (memo[n]) return memo[n];
+    
+    memo[n] = n * calculateFactorial(n - 1, memo);
+    return memo[n];
+}
+
 module.exports = calculateFactorial;
