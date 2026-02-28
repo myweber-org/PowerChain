67b7e363dc9a762450d1eb7cd90950ac96b06c6a
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-28 20:31:59 +0000

    Add input sanitization function to prevent XSS attacks

--- ui/js/dataSanitizer.js
+++ ui/js/dataSanitizer.js
@@ -1,68 +1,30 @@
 function sanitizeInput(input) {
-  const div = document.createElement('div');
-  div.textContent = input;
-  return div.innerHTML;
+    const div = document.createElement('div');
+    div.textContent = input;
+    return div.innerHTML;
 }
 
-function validateEmail(email) {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
+function validateAndSanitize(userInput) {
+    if (typeof userInput !== 'string') {
+        return '';
+    }
+    
+    const trimmedInput = userInput.trim();
+    if (trimmedInput.length === 0) {
+        return '';
+    }
+    
+    const sanitized = sanitizeInput(trimmedInput);
+    const dangerousPatterns = /<script|javascript:|on\w+\s*=|eval\(|alert\(/gi;
+    
+    if (dangerousPatterns.test(sanitized)) {
+        console.warn('Potentially dangerous input detected and neutralized');
+        return sanitized.replace(dangerousPatterns, match => {
+            return match.replace(/./g, '&#x' + match.charCodeAt(0).toString(16) + ';');
+        });
+    }
+    
+    return sanitized;
 }
 
-function escapeHtml(text) {
-  const map = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#039;'
-  };
-  return text.replace(/[&<>"']/g, function(m) { return map[m]; });
-}
-
-module.exports = { sanitizeInput, validateEmail, escapeHtml };function sanitizeInput(input) {
-  if (typeof input !== 'string') {
-    return '';
-  }
-  const map = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#x27;',
-    '/': '&#x2F;'
-  };
-  const reg = /[&<>"'/]/ig;
-  return input.replace(reg, (match) => map[match]);
-}
-
-function validateEmail(email) {
-  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return re.test(email);
-}
-
-module.exports = { sanitizeInput, validateEmail };function sanitizeInput(input) {
-  if (typeof input !== 'string') {
-    return '';
-  }
-  const map = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#x27;',
-    '/': '&#x2F;'
-  };
-  const reg = /[&<>"'/]/ig;
-  return input.replace(reg, (match) => map[match]);
-}
-
-function validateEmail(email) {
-  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return re.test(email);
-}
-
-module.exports = {
-  sanitizeInput,
-  validateEmail
-};
+export { validateAndSanitize };
