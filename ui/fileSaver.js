884e076182fbed75154f2402ee2cd3ed0f0b4d48
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-28 02:03:52 +0000

    Add utility function to save text content as a file with a specified filename

---
+++ ui/fileSaver.js
@@ -0,0 +1,11 @@
+function saveTextAsFile(content, filename) {
+    const blob = new Blob([content], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
