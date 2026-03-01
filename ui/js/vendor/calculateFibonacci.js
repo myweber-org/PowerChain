0d6bb355a0036ccb6a9a5e2a0c41805db474168d
InkspillVerse <inkspill.verse@outlook.com>
2026-03-01 23:29:21 +0000

    Add function to calculate Fibonacci sequence up to a given number

--- ui/js/vendor/calculateFibonacci.js
+++ ui/js/vendor/calculateFibonacci.js
@@ -9,4 +9,15 @@ function generateFibonacci(limit) {
         sequence.push(next);
     }
     return sequence;
+}function calculateFibonacci(limit) {
+    if (limit <= 0) return [];
+    if (limit === 1) return [0];
+    
+    const sequence = [0, 1];
+    while (true) {
+        const nextValue = sequence[sequence.length - 1] + sequence[sequence.length - 2];
+        if (nextValue > limit) break;
+        sequence.push(nextValue);
+    }
+    return sequence;
 }
