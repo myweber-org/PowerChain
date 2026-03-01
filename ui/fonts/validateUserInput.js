3415455493d180b5c29ec618f57d5964495247f2
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-01 21:55:54 +0000

    Add input validation function to sanitize user-provided strings

--- ui/fonts/validateUserInput.js
+++ ui/fonts/validateUserInput.js
@@ -74,4 +74,37 @@ function validateUserInput(username, email) {
     };
 }
 
-export { validateUserInput, validateUsername, validateEmail };
+export { validateUserInput, validateUsername, validateEmail };function sanitizeInput(input) {
+  if (typeof input !== 'string') {
+    return '';
+  }
+  
+  return input
+    .replace(/[<>]/g, '')
+    .trim()
+    .substring(0, 255);
+}
+
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+  if (password.length < 8) {
+    return false;
+  }
+  
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumbers = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+  
+  return hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar;
+}
+
+module.exports = {
+  sanitizeInput,
+  validateEmail,
+  validatePassword
+};
