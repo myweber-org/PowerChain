6a614e6a41ad2c6acc81ad4edeb2d09ccc00a8bf
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-03-03 20:45:45 +0000

    Add input validation function to sanitize user-provided strings

--- ui/build/validateUserInput.js
+++ ui/build/validateUserInput.js
@@ -24,4 +24,36 @@ function validateUserInput(username, password) {
     }
     
     return { username, email, isValid: true };
+}function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    return input.trim()
+        .replace(/[<>]/g, '')
+        .substring(0, 255);
 }
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    
+    return password.length >= minLength && 
+           hasUpperCase && 
+           hasLowerCase && 
+           hasNumbers && 
+           hasSpecialChar;
+}
+
+module.exports = {
+    sanitizeInput,
+    validateEmail,
+    validatePassword
+};
