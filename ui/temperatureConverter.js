783185a6445dfbb2e9d5c4c885c386502455c110
ElaraWrites <elara.writes@outlook.com>
2026-03-03 14:44:49 +0000

    Add a simple Celsius to Fahrenheit conversion function with input validation

--- ui/temperatureConverter.js
+++ ui/temperatureConverter.js
@@ -9,4 +9,42 @@ function fahrenheitToCelsius(fahrenheit) {
 module.exports = {
     celsiusToFahrenheit,
     fahrenheitToCelsius
+};function celsiusToFahrenheit(celsius) {
+    if (typeof celsius !== 'number' || isNaN(celsius)) {
+        throw new Error('Input must be a valid number');
+    }
+    return (celsius * 9/5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    if (typeof fahrenheit !== 'number' || isNaN(fahrenheit)) {
+        throw new Error('Input must be a valid number');
+    }
+    return (fahrenheit - 32) * 5/9;
+}
+
+function convertTemperature(value, unit) {
+    if (typeof value !== 'number' || isNaN(value)) {
+        throw new Error('Value must be a valid number');
+    }
+    
+    if (unit.toLowerCase() === 'c') {
+        return {
+            celsius: value,
+            fahrenheit: celsiusToFahrenheit(value)
+        };
+    } else if (unit.toLowerCase() === 'f') {
+        return {
+            celsius: fahrenheitToCelsius(value),
+            fahrenheit: value
+        };
+    } else {
+        throw new Error('Unit must be "C" or "F"');
+    }
+}
+
+module.exports = {
+    celsiusToFahrenheit,
+    fahrenheitToCelsius,
+    convertTemperature
 };
