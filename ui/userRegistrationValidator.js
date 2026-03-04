23e788e71ff86ee6b058be42cdd76815da26c784
heyQuim <hey.quim@hotmail.com>
2026-03-04 01:32:08 +0000

    Add user registration validation with email format and password strength checks

--- ui/userRegistrationValidator.js
+++ ui/userRegistrationValidator.js
@@ -188,4 +188,45 @@ module.exports = { validateRegistrationData, validateEmail, validatePassword };f
     isValid: Object.keys(errors).length === 0,
     errors: errors
   };
+}function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
 }
+
+function validatePassword(password) {
+    if (password.length < 8) return false;
+    
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    
+    return hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar;
+}
+
+function validateRegistration(userData) {
+    const errors = {};
+    
+    if (!validateEmail(userData.email)) {
+        errors.email = 'Invalid email format';
+    }
+    
+    if (!validatePassword(userData.password)) {
+        errors.password = 'Password must be at least 8 characters with uppercase, lowercase, number and special character';
+    }
+    
+    if (userData.password !== userData.confirmPassword) {
+        errors.confirmPassword = 'Passwords do not match';
+    }
+    
+    if (userData.age && (userData.age < 13 || userData.age > 120)) {
+        errors.age = 'Age must be between 13 and 120';
+    }
+    
+    return {
+        isValid: Object.keys(errors).length === 0,
+        errors: errors
+    };
+}
+
+export { validateRegistration, validateEmail, validatePassword };
