e9f92516a635151a2eaf677811c35accbf7f8d2d
ElaraWrites <elara.writes@outlook.com>
2026-03-06 15:15:41 +0000

    Add data sanitization utility to prevent XSS attacks

--- contracts/dataSanitizer.js
+++ contracts/dataSanitizer.js
@@ -74,4 +74,33 @@ function escapeHtml(text) {
   return div.innerHTML;
 }
 
+export { sanitizeInput, validateEmail, escapeHtml };function sanitizeInput(input) {
+  if (typeof input !== 'string') {
+    return '';
+  }
+  
+  const map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#x27;',
+    '/': '&#x2F;'
+  };
+  
+  const reg = /[&<>"'/]/ig;
+  return input.replace(reg, (match) => map[match]);
+}
+
+function validateEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+}
+
+function escapeHtml(text) {
+  const div = document.createElement('div');
+  div.textContent = text;
+  return div.innerHTML;
+}
+
 export { sanitizeInput, validateEmail, escapeHtml };
