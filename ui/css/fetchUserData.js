be794d0030dbfb55a5151532f7a0216d631ae83c
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-06 19:21:46 +0000

    Add fetchUserData function to retrieve and process user data from API

--- ui/css/fetchUserData.js
+++ ui/css/fetchUserData.js
@@ -27,4 +27,20 @@ function fetchUserData(userId, maxRetries = 3) {
     }
 
     return attemptFetch();
+}function fetchUserData(userId) {
+    return fetch(`https://api.example.com/users/${userId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('User data fetched:', data);
+            return data;
+        })
+        .catch(error => {
+            console.error('Error fetching user data:', error);
+            throw error;
+        });
 }
