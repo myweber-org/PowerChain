1e2ff496d6d2e8f803a66cd3e3e242e51b1ee8b5
ElaraWrites <elara.writes@outlook.com>
2026-03-07 23:09:03 +0000

    Add input validation for username and password fields

--- js/validateUserInput.js
+++ js/validateUserInput.js
@@ -63,4 +63,42 @@ function validateUserInput(username, email) {
     };
 }
 
-export { validateUserInput, validateUsername, validateEmail };
+export { validateUserInput, validateUsername, validateEmail };function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    
+    return password.length >= minLength && 
+           hasUpperCase && 
+           hasLowerCase && 
+           hasNumbers && 
+           hasSpecialChar;
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+    
+    if (!usernameValid && !passwordValid) {
+        return { valid: false, message: 'Invalid username and password' };
+    }
+    
+    if (!usernameValid) {
+        return { valid: false, message: 'Invalid username format' };
+    }
+    
+    if (!passwordValid) {
+        return { valid: false, message: 'Password does not meet requirements' };
+    }
+    
+    return { valid: true, message: 'Validation successful' };
+}
+
+module.exports = { validateUserInput, validateUsername, validatePassword };
