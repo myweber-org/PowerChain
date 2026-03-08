309d411f5cb7dcf25b194de1d49e6b2aa1a8a271
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-03-08 21:03:14 +0000

    Add function to compute the nth Fibonacci number using memoization

--- contracts/calculateFibonacci.js
+++ contracts/calculateFibonacci.js
@@ -83,4 +83,9 @@ function displayFibonacci() {
     
     const fibSequence = calculateFibonacci(n);
     resultDiv.textContent = `Fibonacci sequence (${n} terms): ${fibSequence.join(', ')}`;
+}function calculateFibonacci(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = calculateFibonacci(n - 1, memo) + calculateFibonacci(n - 2, memo);
+    return memo[n];
 }
