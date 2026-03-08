c97acd6b83f752cd6b7ab1c7b139b6ea69d0faec
ElaraWrites <elara.writes@outlook.com>
2026-03-08 23:02:10 +0000

    Implement a function to generate Fibonacci sequence up to n terms

--- ui/css/calculateFibonacci.js
+++ ui/css/calculateFibonacci.js
@@ -7,4 +7,12 @@ function calculateFibonacci(n) {
         fib.push(fib[i - 1] + fib[i - 2]);
     }
     return fib;
+}function generateFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    let sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence;
 }
