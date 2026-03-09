a4171e774df7748e01f1e56665e8314f75ed843c
InkspillVerse <inkspill.verse@outlook.com>
2026-03-09 01:20:36 +0000

    Add random color generator utility

--- ui/css/randomColorGenerator.js
+++ ui/css/randomColorGenerator.js
@@ -1,69 +1,4 @@
 function generateRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function generateRandomRGBColor() {
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  return `rgb(${r}, ${g}, ${b})`;
-}
-
-function generateRandomHSLColor() {
-  const h = Math.floor(Math.random() * 361);
-  const s = Math.floor(Math.random() * 101);
-  const l = Math.floor(Math.random() * 101);
-  return `hsl(${h}, ${s}%, ${l}%)`;
-}
-
-export { generateRandomColor, generateRandomRGBColor, generateRandomHSLColor };
-function generateRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function generateRandomRgbColor() {
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  return `rgb(${r}, ${g}, ${b})`;
-}
-
-function generateRandomHslColor() {
-  const h = Math.floor(Math.random() * 360);
-  const s = Math.floor(Math.random() * 101);
-  const l = Math.floor(Math.random() * 101);
-  return `hsl(${h}, ${s}%, ${l}%)`;
-}
-
-export { generateRandomColor, generateRandomRgbColor, generateRandomHslColor };
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function generateRandomColors(count) {
-  const colors = [];
-  for (let i = 0; i < count; i++) {
-    colors.push(getRandomColor());
-  }
-  return colors;
-}
-
-export { getRandomColor, generateRandomColors };function generateRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
@@ -72,18 +7,18 @@ export { getRandomColor, generateRandomColors };function generateRandomColor() {
     return color;
 }
 
-function generateRandomRgbColor() {
+function generateRandomRGB() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`;
 }
 
-function generateRandomHslColor() {
-    const h = Math.floor(Math.random() * 360);
+function generateRandomHSL() {
+    const h = Math.floor(Math.random() * 361);
     const s = Math.floor(Math.random() * 101);
     const l = Math.floor(Math.random() * 101);
     return `hsl(${h}, ${s}%, ${l}%)`;
 }
 
-export { generateRandomColor, generateRandomRgbColor, generateRandomHslColor };
+export { generateRandomColor, generateRandomRGB, generateRandomHSL };
