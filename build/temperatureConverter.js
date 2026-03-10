33d788b1d117f76e76c860571db3df506e3f2d8f
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-03-10 20:15:47 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and vice versa

--- build/temperatureConverter.js
+++ build/temperatureConverter.js
@@ -1,26 +1,4 @@
 function celsiusToFahrenheit(celsius) {
-  return (celsius * 9/5) + 32;
-}
-
-function fahrenheitToCelsius(fahrenheit) {
-  return (fahrenheit - 32) * 5/9;
-}
-
-function convertTemperature(value, unit) {
-  if (unit.toLowerCase() === 'c') {
-    return celsiusToFahrenheit(value);
-  } else if (unit.toLowerCase() === 'f') {
-    return fahrenheitToCelsius(value);
-  } else {
-    throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
-  }
-}
-
-module.exports = {
-  celsiusToFahrenheit,
-  fahrenheitToCelsius,
-  convertTemperature
-};function celsiusToFahrenheit(celsius) {
     return (celsius * 9/5) + 32;
 }
 
@@ -38,8 +16,22 @@ function convertTemperature(value, unit) {
     }
 }
 
+function formatTemperature(value, originalUnit) {
+    const convertedUnit = originalUnit.toLowerCase() === 'c' ? 'F' : 'C';
+    return `${value.toFixed(2)}°${convertedUnit}`;
+}
+
+function validateTemperatureInput(value) {
+    if (typeof value !== 'number' || isNaN(value)) {
+        return false;
+    }
+    return true;
+}
+
 module.exports = {
     celsiusToFahrenheit,
     fahrenheitToCelsius,
-    convertTemperature
+    convertTemperature,
+    formatTemperature,
+    validateTemperatureInput
 };
