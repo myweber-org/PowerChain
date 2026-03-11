5798bf901359e40ca4939c03c26450addf321fd6
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-11 19:05:33 +0000

    Add function to calculate the nth Fibonacci number using memoization

--- ui/fonts/calculateFibonacci.js
+++ ui/fonts/calculateFibonacci.js
@@ -40,4 +40,9 @@ displayFibonacci();function generateFibonacci(limit) {
         sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
     }
     return sequence;
+}function calculateFibonacci(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = calculateFibonacci(n - 1, memo) + calculateFibonacci(n - 2, memo);
+    return memo[n];
 }
