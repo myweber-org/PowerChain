fa1b59c4882fed57a640fb39824ffe83d9f8f9cb
InkspillVerse <inkspill.verse@outlook.com>
2026-03-12 00:02:53 +0000

    Add email validation utility with regex pattern

--- ui/fonts/validateEmail.js
+++ ui/fonts/validateEmail.js
@@ -1,6 +1,6 @@
 function validateEmail(email) {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
 }
 
 module.exports = validateEmail;
